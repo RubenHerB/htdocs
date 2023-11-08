42485f53a512cce9d61d9bff11e694367b00f2be
RubenHerB <rubenhb19@educastur.es>
2023-11-08 09:28:42 +0000

    8 nov 2023, 10:28

--- Ejercicios_php/Ejemplos/Sesiones/Simon/Inicio.php
+++ Ejercicios_php/Ejemplos/Sesiones/Simon/Inicio.php
@@ -18,7 +18,7 @@ body{background-color: aquamarine;}
 
 <?php
     function pintar($c){
-        $col=["black","red","blue","yellow","green"];
+        $col=["black","red","#4772ff","yellow","green"];
         foreach($c as $p)
         echo "<span class=\"dot\" style=\"background-color:",$col[$p],"\"></span>";
       }
