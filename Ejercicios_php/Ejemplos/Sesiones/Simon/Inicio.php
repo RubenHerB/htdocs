94ce90edc98fc1e7b11ffefd6852a915bca9c718
RubenHerB <rubenhb19@educastur.es>
2023-11-08 09:01:33 +0000

    8 nov 2023, 10:01

---
+++ Ejercicios_php/Ejemplos/Sesiones/Simon/Inicio.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simon</title>
+  <style>
+    .dot {
+    aspect-ratio: 1/1;
+  width: 20%;
+  border-radius: 50%;
+  display: inline-block;
+  margin-left: 4%;
+  margin-top: 100px;
+}
+body{background-color: aquamarine;}
+  </style>
+</head>
+<body>
+<?php
+    function pintar($c1,$c2,$c3,$c4)     {
+        $c=array($c1,$c2,$c3,$c4);
+        shuffle($c);
+        foreach($c as $p)
+        echo "<span class=\"dot\" style=\"background-color:$p\"></span>";
+      }
+      session_start();
+      const col=["red","blue","yellow","green"];
+?>   
+</body>
+</html>
