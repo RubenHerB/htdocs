fe0a3aecd0659c3a1b24bbcb85ca1034b90aae27
RubenHerB <rubenhb19@educastur.es>
2023-11-10 10:49:51 +0000

    10 nov 2023, 11:49

--- Ejercicios_php/Ejemplos/Sesiones/Simon/circulos.php
+++ Ejercicios_php/Ejemplos/Sesiones/Simon/circulos.php
@@ -7,7 +7,7 @@ class Circulos{
     function pintar($c){
         $col=["black","red","#4772ff","yellow","green"];
         foreach($c as $p)
-        echo "<span class=\"dot\" id=\"dot\" style=\"background-color:",$col[$p],"\"></span>";
+        echo "<span class=\"dot\" id=\"dot\" style=\"background-color:",$col[$p],";width=",(90/count($c)),"%\"></span>";
       }
 }
-?>
+?> 
