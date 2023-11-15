b758ec0b013cfc9912493d588463bef1fd890378
RubenHerB <rubenhb19@educastur.es>
2023-11-15 09:10:09 +0000

    15 nov 2023, 10:10

---
+++ Ejercicios_php/Unidad_4/Practica5/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sesiones</title>
+</head>
+<body>
+    <form method="post" action="confirmacion.php">
+      Usuario: 
+      <input type="text" name="user">
+      <br/>
+      Contraseña: 
+      <input type="passwrd" name="pasword">
+      <br/>
+      <input type="submit">
+
+</body>
+</html>
