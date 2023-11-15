930ee5031d1b7d273f1631004b1a8989085c0318
RubenHerB <rubenhb19@educastur.es>
2023-11-15 08:41:22 +0000

    15 nov 2023, 9:41

---
+++ Ejercicios_php/Unidad_4/Practica4U4.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cookies</title>
+</head>
+<body>
+    <?php
+        setcookie('color', '', time()+600, '/', '127.0.0.1', FALSE, FALSE);
+    ?>
+
+    </div>
+</body>
+</html>
