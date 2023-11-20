35f3d76edfbab318882ce797da084f75370398be
RubenHerB <rubenhb19@educastur.es>
2023-11-20 09:05:26 +0000

    20 nov 2023, 10:05

---
+++ Ejercicios_php/Unidad_4/Practica2/Practica2U4_4.php
@@ -0,0 +1,9 @@
+<form method="post" >
+<input type="radio" name="sexo"
+<?php if (isset($sexo) && $sexo=="mujer") echo "checked";?>
+value="mujer"> Mujer
+<input type="radio" name="sexo"
+<?php if (isset($sexo) && $sexo=="hombre") echo "checked";?>
+value="hombre"> Hombre
+<span class="error">* <?php echo $sexoErr;?></span><br><br>
+</form>
