031c3999c07f5342ce67ea2a65b0afff18534cc3
RubenHerB <rubenhb19@educastur.es>
2023-11-20 10:05:08 +0000

    20 nov 2023, 11:05

--- Ejercicios_php/Unidad_4/Practica2/Practica2U4_11.php
+++ Ejercicios_php/Unidad_4/Practica2/Practica2U4_11.php
@@ -52,7 +52,7 @@ E-mail:<input type="text" name="email" value="<?php echo $email; ?>">
 <span class="error">* <?php echo $emailErr;?></span><br><br>
 Website:<input type="text" name="url" value="<?php echo $url; ?>">
 <span class="error">* <?php echo $urlErr;?></span><br><br>
-Comment:<input style="height:100px;width:300px" type="text" name="comment" value="<?php echo $com; ?>">
+Comment:<textarea style="height:100px;width:300px" name="comment" value="<?php echo $com; ?>"></textarea>
 <br><br>
 <input type="submit">
 
