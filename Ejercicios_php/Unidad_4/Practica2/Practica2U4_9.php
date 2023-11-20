689718092dea9906419ac0963f3e3f966218261f
RubenHerB <rubenhb19@educastur.es>
2023-11-20 09:33:19 +0000

    20 nov 2023, 10:33

---
+++ Ejercicios_php/Unidad_4/Practica2/Practica2U4_9.php
@@ -0,0 +1,13 @@
+<?php
+function validar_email($email){
+$emailErr="Email correcto";
+if (empty($email)) {
+ $emailErr = "Se requiere Email";
+ } else {
+ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+ $emailErr = "Fomato de Email invalido";
+ }
+ }
+return $emailErr;
+}
+?>
