09502fd512644939fa2234898c8690e453923a16
RubenHerB <rubenhb19@educastur.es>
2023-11-20 09:20:15 +0000

    20 nov 2023, 10:20

--- Ejercicios_php/Unidad_4/Practica2/Practica2U4_6.php
+++ Ejercicios_php/Unidad_4/Practica2/Practica2U4_6.php
@@ -1,4 +1,14 @@
 <?php
+
+$email=$_POST['email'];
+$emailErr="Email correcto";
+if (empty($email)) {
+ $emailErr = "Se requiere Email";
+ } else {
+ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+ $emailErr = "Fomato de Email invalido";
+ }
+ }
 ?>
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 mail: <input type="text" name="email" value="<?php echo $email;?>">
