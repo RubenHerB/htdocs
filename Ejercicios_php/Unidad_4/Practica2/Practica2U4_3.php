acd85e5b2369ab67d0ad128aa74738b45f5cf4e3
RubenHerB <rubenhb19@educastur.es>
2023-11-20 09:01:12 +0000

    20 nov 2023, 10:01

--- Ejercicios_php/Unidad_4/Practica2/Practica2U4_3.php
+++ Ejercicios_php/Unidad_4/Practica2/Practica2U4_3.php
@@ -1,4 +1,4 @@
-<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
  <input type="text" name="1">
  <input type="submit">
 </form>
