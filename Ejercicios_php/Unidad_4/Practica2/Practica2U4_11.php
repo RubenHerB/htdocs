975b2523e7d983d2c961223698dd538a3ba6ef49
RubenHerB <rubenhb19@educastur.es>
2023-11-22 08:41:02 +0000

    22 nov 2023, 9:41

--- Ejercicios_php/Unidad_4/Practica2/Practica2U4_11.php
+++ Ejercicios_php/Unidad_4/Practica2/Practica2U4_11.php
@@ -56,34 +56,8 @@ if(isset($_POST['sexo'])){
  <style>
     .error {
   color: red;
-  animation-name: parpadeo;
-  animation-duration: 1s;
-  animation-timing-function: linear;
-  animation-iteration-count: infinite;
-
-  -webkit-animation-name:parpadeo;
-  -webkit-animation-duration: 1s;
-  -webkit-animation-timing-function: linear;
-  -webkit-animation-iteration-count: infinite;
 }
 
-@-moz-keyframes parpadeo{  
-  0% { opacity: 1.0; }
-  50% { opacity: 0.0; }
-  100% { opacity: 1.0; }
-}
-
-@-webkit-keyframes parpadeo {  
-  0% { opacity: 1.0; }
-  50% { opacity: 0.0; }
-   100% { opacity: 1.0; }
-}
-
-@keyframes parpadeo {  
-  0% { opacity: 1.0; }
-   50% { opacity: 0.0; }
-  100% { opacity: 1.0; }
-}
  </style>
  <h1>Form validation example</h1>
  <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
