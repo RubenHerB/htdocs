31d0d2f46b7cc5a942b4907483ed0095095727a6
RubenHerB <rubenhb19@educastur.es>
2023-11-22 09:39:22 +0000

    22 nov 2023, 10:39

---
+++ Ejercicios_php/Ejemplos/Ficheros/Ejemplo1.php
@@ -0,0 +1,10 @@
+<?php // testfile.php
+ $fh = fopen("testfile.txt", 'w') or die("Failed to create file");
+ $text = "Line 1
+Line 2
+Line 3
+";
+ fwrite($fh, $text) or die("Could not write to file");
+ fclose($fh);
+ echo "File 'testfile.txt' written successfully";
+?>
