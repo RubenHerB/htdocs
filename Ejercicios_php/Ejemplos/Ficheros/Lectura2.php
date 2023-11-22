c4b2c0060fd7d6b46b94b6f171ea3e2a87cbd846
RubenHerB <rubenhb19@educastur.es>
2023-11-22 09:56:47 +0000

    22 nov 2023, 10:56

--- Ejercicios_php/Ejemplos/Ficheros/Lectura2.php
+++ Ejercicios_php/Ejemplos/Ficheros/Lectura2.php
@@ -6,5 +6,12 @@ if (!$fp) {
 while (false !== ($carácter = fgetc($fp))) {
     echo "$carácter\n";
 }
-echo 'br'
+echo '<br/>';
+$fp = fopen('testfile.txt', 'r');
+if (!$fp) {
+    echo 'No se pudo abrir archivo.txt';
+}
+while (false !== ($carácter = fgets($fp))) {
+    echo "$carácter ";
+}
 ?>
