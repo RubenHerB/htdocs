0cde41101c57790582c853262c321afbc32ddfbe
RubenHerB <rubenhb19@educastur.es>
2023-11-24 12:12:11 +0000

    24 nov 2023, 13:12

--- Ejercicios_php/Unidad_4/Practica3/upload.php
+++ Ejercicios_php/Unidad_4/Practica3/upload.php
@@ -2,28 +2,25 @@
 if(isset($_FILES["fileToUpload"])) {
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     if((pathinfo($target_file,PATHINFO_EXTENSION))=='txt'){
         if (!file_exists($target_file)) {
             if ($_FILES["fileToUpload"]["size"] <= 300000) {
-                echo "El fichero subido es correcto";
+                echo "El fichero subido es correcto<br>";
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                    echo "El archivo ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " se ha subido";
+                  } else {
+                    echo "Ha habido un error al subir su archivo";
+                  }
                }else{
                 echo "El fichero es demasiado grande.";
-                $uploadOk = 0;
                }   
             }else{
-            echo "El fichero ya existe.";
-            $uploadOk = 0;}
+            echo "El fichero ya existe.";}
     }else{
         echo "El fichero no esta en formato txt";
-    $uploadOk = 0;
 }   
-if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "El archivo ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " se ha subido";
-  } else {
-    echo "Ha habido un error al subir su archivo";
-  }
+
    }else{
     echo "No se ha subido ningun fichero";
 }
