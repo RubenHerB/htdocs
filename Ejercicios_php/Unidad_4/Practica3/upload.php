9fd3927a3b64040db99d14c23226da101f995e4b
RubenHerB <rubenhb19@educastur.es>
2023-11-24 12:00:01 +0000

    24 nov 2023, 13:00

--- Ejercicios_php/Unidad_4/Practica3/upload.php
+++ Ejercicios_php/Unidad_4/Practica3/upload.php
@@ -1,12 +1,10 @@
 <?php
+if(isset($_POST['fileToUpload']) && $_POST['fileToUpload'] == NULL) {
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Comprobamos si es una imagen real o falsa
- $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
- if($check !== false) {
- echo "El fichero es una imagen - " . $check["mime"] . ".";
  if (!file_exists($target_file)) {
     if((pathinfo('test2.txt',PATHINFO_EXTENSION))=='txt'){
 
@@ -17,8 +15,7 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
    }else{
     echo "El fichero ya existe.";
     $uploadOk = 0;}
- } else {
- echo "El fichero no es una imagen.";
- $uploadOk = 0;
- }
+   }else{
+    echo "No se ha subido ningun fichero";
+}
 ?>
