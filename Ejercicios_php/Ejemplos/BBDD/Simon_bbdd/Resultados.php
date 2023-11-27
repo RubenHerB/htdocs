ae13d09c64fcac09fc8f9a9bfb0677b7b2302a3d
RubenHerB <rubenhb19@educastur.es>
2023-11-27 11:58:48 +0000

    27 nov 2023, 12:58

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Resultados.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Resultados.php
@@ -61,10 +61,10 @@ $result = $connection->query($query);
 $result->data_seek(0);
 $ms=$s=$result->fetch_assoc()['s'];
 
- echo "<table><tr><th>Codigo</th><th>Nombre</th><th>Aciertos</th><th>Grafica</th></tr>";
+ echo "<table><tr><th>Rank</th><th>Codigo</th><th>Nombre</th><th>Aciertos</th><th>Grafica</th></tr>";
  for ($j = 0 ; $j < $rows ; ++$j)
  {
-echo '<tr>';
+echo '<tr><td>'.($j+1).'</td>';
  $result->data_seek($j);
  echo '<td>'.$result->fetch_assoc()['Codigo'].'</td>';
  $result->data_seek($j);
