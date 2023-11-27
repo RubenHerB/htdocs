de47126fe64e4fbe8aa1588a474ba2811671c75b
RubenHerB <rubenhb19@educastur.es>
2023-11-27 13:04:14 +0000

    27 nov 2023, 14:04

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Inicio.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Inicio.php
@@ -154,7 +154,7 @@ echo '<tr><td>'.($j+1).'</td>';
       var n=parseInt(window.some_variable = '<?=$_POST['nc']?>');
       if (!isNaN(n)){
         t=[0,0,0];
-        t[1]=Math.round(Math.pow(1.8,n));
+        t[1]=Math.round(2+Math.pow(1.4,n));
         if(t[1]>=60){
           t[0]=Math.trunc(t[1]/60);
           t[1]-=(Math.trunc(t[1]/60)*60);
