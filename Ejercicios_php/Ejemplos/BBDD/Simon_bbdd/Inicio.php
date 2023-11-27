92101df255e37a3b5e629b88bec526d90102d0ce
RubenHerB <rubenhb19@educastur.es>
2023-11-27 12:17:53 +0000

    27 nov 2023, 13:17

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Inicio.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Inicio.php
@@ -6,7 +6,7 @@
   <style>
 
   .ranks {position: absolute; top: 20px;left: 20px;}
-  .rankcontent{display: none;position: absolute; top: 0;left: 0;height: 40%;width: 40%;border: 1px solid black; background-color: white;}
+  .rankcontent{display: none;position: absolute; top: 0;left: 0;height: 40%;width: 40%;border: 1px solid black; background-color: white;overflow: scroll;}
 
     .dot {
     aspect-ratio: 1/1;
@@ -47,6 +47,42 @@ table{margin: 0 auto 0 auto;}
   </style>
 </head>
 <body>
+  <div class="rankcontent">
+  <h1>Rankings</h1>
+<?php
+$connection = new mysqli('localhost', 'root', '', 'bdsimon');
+if ($connection->connect_error) die("Fatal Error");
+$query = "SELECT 
+u.Codigo, u.Nombre, sum(j.acierto) as s
+FROM
+usuarios u 
+LEFT OUTER JOIN 
+jugadas j ON u.Codigo=j.codigousu
+GROUP BY Nombre
+ORDER BY sum(acierto) DESC, codigousu";
+$result = $connection->query($query);
+ if (!$result) die("Fatal Error");
+ $rows = $result->num_rows; 
+
+$result->data_seek(0);
+$ms=$s=$result->fetch_assoc()['s'];
+
+ echo "<table><tr><th>Rank</th><th>Codigo</th><th>Nombre</th><th>Aciertos</th><th>Grafica</th></tr>";
+ for ($j = 0 ; $j < $rows ; ++$j)
+ {
+echo '<tr><td>'.($j+1).'</td>';
+ $result->data_seek($j);
+ echo '<td>'.$result->fetch_assoc()['Codigo'].'</td>';
+ $result->data_seek($j);
+ echo '<td>'.$result->fetch_assoc()['Nombre'].'</td>';
+ $result->data_seek($j);
+ $s=$result->fetch_assoc()['s'];
+ echo "<td>$s</td><td class= \"grf\"><div style=\"height: 10px;width:".(200*($s/$ms))."px\"></div></td></tr>";
+ }
+ echo "</table>";
+ ?>
+ <br>
+  </div>
     <h1>SIMÓN</h1>
     <?php
     session_start();
