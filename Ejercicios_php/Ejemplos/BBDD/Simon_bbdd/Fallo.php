b3710547754d705b4664f9b813f96de644483f41
RubenHerB <rubenhb19@educastur.es>
2023-11-27 12:42:47 +0000

    27 nov 2023, 13:42

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Fallo.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Fallo.php
@@ -61,9 +61,60 @@ input[type="submit"]:hover{
     transform: translateY(5px);
 }
 body{background-color: aquamarine;text-align: center;}
+
+table,tr,td,th{border-collapse: collapse;border: 1px solid black;background-color: white;padding: 5px;}
+td:not(.grf){text-align: center;
+}
+.grf div{background-color: blue;}
+table{margin: 0 auto 0 auto;}
+.ranks {position: absolute; top: 20px;left: 20px;}
+  .rankcontent{position: absolute; top: 0;left: 0;height: 40%;width: 40%;border: 1px solid black; background-color: white;overflow: scroll;}
   </style>
 </head>
 <body>
+<div class="rankcontent" id="rankcontent" style="display: none;">
+  <h1>Rankings</h1>
+<?php
+$connection = new mysqli('localhost', 'root', '', 'bdsimon');
+if ($connection->connect_error) die("Fatal Error");
+$query = "SELECT 
+u.Codigo, u.Nombre, sum(j.acierto) as s
+FROM
+usuarios u 
+LEFT OUTER JOIN 
+jugadas j ON u.Codigo=j.codigousu
+GROUP BY Nombre
+ORDER BY sum(acierto) DESC, codigousu";
+$result = $connection->query($query);
+ if (!$result) die("Fatal Error");
+ $rows = $result->num_rows; 
+
+$result->data_seek(0);
+$ms=$s=$result->fetch_assoc()['s'];
+
+ echo "<table><tr><th>Rank</th><th>Codigo</th><th>Nombre</th><th>Aciertos</th><th>Grafica</th></tr>";
+ for ($j = 0 ; $j < $rows ; ++$j)
+ {
+echo '<tr><td>'.($j+1).'</td>';
+ $result->data_seek($j);
+ echo '<td>'.$result->fetch_assoc()['Codigo'].'</td>';
+ $result->data_seek($j);
+ echo '<td>'.$result->fetch_assoc()['Nombre'].'</td>';
+ $result->data_seek($j);
+ $s=$result->fetch_assoc()['s'];
+ echo "<td>$s</td><td class= \"grf\"><div style=\"height: 10px;width:".(200*($s/$ms))."px\"></div></td></tr>";
+ }
+ echo "</table>";
+ ?>
+ <br>
+  </div>
+
+
+
+
+
+
+
     <h1>SIMÓN</h1>
     <h2>Has fallado</h2>
     <h3 id="temp"></h3>
@@ -101,9 +152,6 @@ body{background-color: aquamarine;text-align: center;}
     <form method="post" action="Autenticacion.php">
         <input type="submit" value="Volver a jugar?"/>
     </form>
-    <form class="ranks" method="post" action=Resultados.php >
-    <input type="submit" name="submit" value="RANKING"/>
-    </form>
     <script type="text/javascript">
       var t=[localStorage.getItem("timer0"),localStorage.getItem("timer1"),localStorage.getItem("timer2")];
       if(t[0]==0&&t[1]==0&&t[2]==0){
@@ -113,5 +161,11 @@ body{background-color: aquamarine;text-align: center;}
       document.getElementById("temp").innerHTML="Tiempo restante: "+("0" + t[0]).slice(-2)+":"+("0" + t[1]).slice(-2)+":"+("0" + t[2]).slice(-2) ;
       }
     </script>
+    <button type="button" class="ranks" onclick="var x = document.getElementById('rankcontent');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }">RANKING</button> 
 </body>
 </html>
