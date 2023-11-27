32f1477003ff8b9b4521774b678ee748906ae4a0
RubenHerB <rubenhb19@educastur.es>
2023-11-27 11:17:52 +0000

    27 nov 2023, 12:17

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Resultados.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Resultados.php
@@ -1,8 +1,7 @@
 <?php
 $connection = new mysqli('localhost', 'root', '', 'bdsimon');
 if ($connection->connect_error) die("Fatal Error");
-$query = 
-"SELECT usuarios.Codigo, usuarios.Nombre, s.acs FROM usuarios, (SELECT sum(acierto) as acs, codigousu FROM jugadas as s) where usuarios.Codigo = s.codigousu";
+$query = "SELECT usuarios.Codigo, usuarios.Nombre FROM usuarios";
 $result = $connection->query($query);
  if (!$result) die("Fatal Error");
  $rows = $result->num_rows; 
