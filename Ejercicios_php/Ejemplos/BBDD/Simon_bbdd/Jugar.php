15e2e170df7c2bc36da51fb5f6b82ca3b98e5532
RubenHerB <rubenhb19@educastur.es>
2023-12-01 13:10:44 +0000

    1 dic 2023, 14:10

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Jugar.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/Jugar.php
@@ -49,40 +49,7 @@
 
 <div class="ranking3" id="rankcontent" >
   <h1>Ranking</h1>
-<?php
-include("login.php");
-$log=new login();
-$connection=$log->log();
-$query = "SELECT 
-u.Codigo, u.Nombre, sum(j.acierto) as s
-FROM
-usuarios u 
-LEFT OUTER JOIN 
-jugadas j ON u.Codigo=j.codigousu
-GROUP BY Nombre
-ORDER BY sum(acierto) DESC, codigousu";
-$result = $connection->query($query);
- if (!$result) die("Fatal Error");
- $rows = $result->num_rows; 
-
-$result->data_seek(0);
-$ms=$s=$result->fetch_assoc()['s'];
-
- echo "<table><tr><th>Rank</th><th>Codigo</th><th>Nombre</th><th>Aciertos</th><th>Grafica</th></tr>";
- for ($j = 0 ; $j < $rows ; ++$j)
- {
-echo '<tr><td>'.($j+1).'</td>';
- $result->data_seek($j);
- echo '<td>'.$result->fetch_assoc()['Codigo'].'</td>';
- $result->data_seek($j);
- echo '<td>'.$result->fetch_assoc()['Nombre'].'</td>';
- $result->data_seek($j);
- $s=$result->fetch_assoc()['s'];
- if($s==null){$s=0;}
- echo "<td>$s</td><td class= \"grf\"><div style=\"height: 10px;width:".(200*($s/$ms))."px\"></div></td></tr>";
- }
- echo "</table>";
- ?>
+  <?php include 'ranking.php';?>
  <br>
   </div>
 
