1dd932248af3cc3e3fe5322532cca2737347a373
RubenHerB <rubenhb19@educastur.es>
2023-12-01 12:39:36 +0000

    1 dic 2023, 13:39

--- Ejercicios_php/Ejemplos/BBDD/hash.php
+++ Ejercicios_php/Ejemplos/BBDD/hash.php
@@ -2,6 +2,7 @@
 var_dump(password_hash('afgadgb', PASSWORD_DEFAULT));echo '<br/>';
 var_dump(password_hash('yolanda', PASSWORD_DEFAULT));echo '<br/>';
 var_dump(password_hash('ruben', PASSWORD_DEFAULT));echo '<br/>';
+var_dump(hash('sha512','ruben'));echo '<br/>';
 
 
 ?>
