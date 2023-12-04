63b3ef6a85e326d6917b4881cbf427555a335316
RubenHerB <rubenhb19@educastur.es>
2023-12-04 09:50:24 +0000

    4 dic 2023, 10:50

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/jugadas.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/jugadas.php
@@ -11,7 +11,6 @@
 <div>
     <?php 
     if(isset($_POST['submit'])) {
-        include('login.php');
         if($_POST['submit'] == "Exportat archivo del servidor"){
             $fh = fopen("jugadas.txt", 'w') or die("Failed to create file");
             $text="";
@@ -39,20 +38,22 @@
             $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
             $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
                 if((pathinfo($target_file,PATHINFO_EXTENSION))=='txt'){
-                    if (!file_exists($target_file)) {
                         if ($_FILES["fileToUpload"]["size"] <= 300000) {
                             echo "El fichero subido es correcto<br>";
                             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
                                 echo "El archivo ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " se ha subido";
-                                header("Location: upload.php");
+                                
+                                include('login.php');
+
+
+
                               } else {
                                 echo "Ha habido un error al subir su archivo";
                               }
                            }else{
                             echo "El fichero es demasiado grande.";
                            }   
-                        }else{
-                        echo "El fichero ya existe.";}
+                        
                 }else{
                     echo "El fichero no esta en formato txt";
             }   
