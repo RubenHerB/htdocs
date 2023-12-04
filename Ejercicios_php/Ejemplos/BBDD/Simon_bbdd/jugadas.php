3bfd7d4c1420658de29e1c1b651b725d2f70279f
RubenHerB <rubenhb19@educastur.es>
2023-12-04 09:10:03 +0000

    4 dic 2023, 10:10

---
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/jugadas.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simon</title>
+  <link rel="stylesheet" href="simon.css">
+  <link rel="icon" type="image/png" href="favicon.png">
+</head>
+<body>
+</body>
+</html>
