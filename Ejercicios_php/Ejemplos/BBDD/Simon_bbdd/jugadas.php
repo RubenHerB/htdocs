26f436afd0e2f8ec5c0b8ab1ee8fe24ec017140e
RubenHerB <rubenhb19@educastur.es>
2023-12-04 09:44:57 +0000

    4 dic 2023, 10:44

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/jugadas.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/jugadas.php
@@ -34,7 +34,31 @@
 
         }
     }elseif($_POST['submit'] == "Importat archivo"){
-
+        if(isset($_FILES["fileToUpload"])) {
+            $target_dir = "";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+                if((pathinfo($target_file,PATHINFO_EXTENSION))=='txt'){
+                    if (!file_exists($target_file)) {
+                        if ($_FILES["fileToUpload"]["size"] <= 300000) {
+                            echo "El fichero subido es correcto<br>";
+                            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                                echo "El archivo ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " se ha subido";
+                              } else {
+                                echo "Ha habido un error al subir su archivo";
+                              }
+                           }else{
+                            echo "El fichero es demasiado grande.";
+                           }   
+                        }else{
+                        echo "El fichero ya existe.";}
+                }else{
+                    echo "El fichero no esta en formato txt";
+            }   
+            
+               }else{
+                echo "No se ha subido ningun fichero";
+            }
     }
     ?>
 </div>
