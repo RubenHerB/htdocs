8a4eac52a818dd1618d1de3d9e000e5458db2a3e
RubenHerB <rubenhb19@educastur.es>
2023-12-04 09:17:24 +0000

    4 dic 2023, 10:17

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/jugadas.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/jugadas.php
@@ -7,8 +7,24 @@
   <link rel="icon" type="image/png" href="favicon.png">
 </head>
 <body>
+<h1>Importacion/Exportacion de las jugadas</h1>
+<div>
+    <?php 
+    include('login.php');
+    if(isset($_POST['submit'])) {
+        if($_POST['submit'] == "Exportat archivo del servidor"){
+            $fh = fopen("testfile.txt", 'w') or die("Failed to create file");
+
+        }
+
+    ?>
+</div>
+
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     <input type="submit" name="submit" value="Exportat archivo del servidor" />
     </form>
+
+
+
 </body>
 </html>
