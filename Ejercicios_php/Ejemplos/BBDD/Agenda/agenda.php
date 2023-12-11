033bf7e070878a430d77c81fb3ae13f9284ddc9d
RubenHerB <rubenhb19@educastur.es>
2023-12-11 09:55:56 +0000

    11 dic 2023, 10:55

---
+++ Ejercicios_php/Ejemplos/BBDD/Agenda/agenda.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Agenda</title>
+
+</head>
+<body>
+    <h1>AGENDA</h1>
+    <h2>Hola <?php session_start(); echo $_SESSION['usu']; ?></h2>
+</body>
+</html>
