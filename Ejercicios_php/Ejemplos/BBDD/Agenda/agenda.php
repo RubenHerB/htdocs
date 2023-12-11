2df361987ea7e37b7ca8d4a0f2eb27bc73b171bd
RubenHerB <rubenhb19@educastur.es>
2023-12-11 10:09:01 +0000

    11 dic 2023, 11:09

--- Ejercicios_php/Ejemplos/BBDD/Agenda/agenda.php
+++ Ejercicios_php/Ejemplos/BBDD/Agenda/agenda.php
@@ -14,7 +14,16 @@
     <h2>Hola <?php session_start(); echo $_SESSION['usu']; ?></h2>
     <form method="post" action="<
     <?php 
-    fo
+    for($i=1; $i<=$_SESSION['con'];$i++){
+        echo <<<_END
+            <div>
+            <h3>Contacto $i</h3>
+            Nombre $i: <input type="text" name="nombre$i" required>
+            Email $i: <input type="text" name="mail$i" required>
+            Telèfono $i: <input type="text" name="tel$i" required>
+            </div>
+        _END;
+    }
     ?>
 </body>
 </html>
