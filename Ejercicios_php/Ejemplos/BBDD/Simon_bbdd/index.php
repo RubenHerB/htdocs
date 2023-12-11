8f3865dd6d9203825c8291f286de06a97168c351
RubenHerB <rubenhb19@educastur.es>
2023-12-11 08:47:02 +0000

    11 dic 2023, 9:47

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/index.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/index.php
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST) && isset($_POST["usern
         if(password_verify($password, $row['Clave'])){
           $l=true;
           $c=$row["Codigo"];
-          $ad=$row['Rol']=1? true:false;
+          $ad=$row['Rol']==1? true:false;
         }
     }    
 
