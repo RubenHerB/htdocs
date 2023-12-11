be6a06b14dd226ac29034dad009c968d64cb05c0
RubenHerB <rubenhb19@educastur.es>
2023-12-11 11:11:35 +0000

    11 dic 2023, 12:11

--- Ejercicios_php/Ejemplos/BBDD/Agenda/totales.php
+++ Ejercicios_php/Ejemplos/BBDD/Agenda/totales.php
@@ -1,5 +1,14 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Agenda</title>
 
-<?php
+</head>
+<body>
+    <h1>AGENDA</h1>
+    <div>
+    Hola <?php session_start(); echo $_SESSION['usu']."<br>"; 
 $connection = new mysqli('localhost', 'root','', 'AGENDA');
 if ($connection->connect_error) die("Fatal Error");
 $query = "SELECT 
@@ -16,7 +25,7 @@ $result = $connection->query($query);
 $result->data_seek(0);
 $ms=$result->fetch_assoc()['s'];
 
- echo "<table><tr><th>Rank</th><th>Codigo</th><th>Nombre</th><th>Aciertos</th><th>Grafica</th></tr>";
+ echo "<table><tr><th>Codigo usuario</th><th>Nombre</th><th>Numero de contactos</th><th>Grafica</th></tr>";
  for ($j = 0 ; $j < $rows ; ++$j)
  {
 echo '<tr><td>'.($j+1).'</td>';
@@ -25,7 +34,7 @@ echo '<tr><td>'.($j+1).'</td>';
  $result->data_seek($j);
  echo '<td>'.$result->fetch_assoc()['Nombre'].'</td>';
  $result->data_seek($j);
- $s=$result->fetch_assoc()['s'];
+ $s=$result->fetch_assoc()['co'];
  if($s==null){$s=0;}
  echo "<td>$s</td><td class= \"grf\"><div style=\"height: 10px;width:".(200*($s/$ms))."px\"></div></td></tr>";
  }
