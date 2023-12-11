1e3f5399f9818c9dbec45bb69c6a4020a3a64395
RubenHerB <rubenhb19@educastur.es>
2023-12-11 09:51:01 +0000

    11 dic 2023, 10:51

--- Ejercicios_php/Ejemplos/BBDD/Agenda/inicio.php
+++ Ejercicios_php/Ejemplos/BBDD/Agenda/inicio.php
@@ -15,8 +15,9 @@
     </div>
     <div>
       <?php
-      $order=shuffle([0,1,2,3,4]);
-      for ($i=1;$i<$_SESSION['con'];$i++){
+      $order=[0,1,2,3,4];
+      shuffle($order);
+      for ($i=0;$i<$_SESSION['con'];$i++){
         echo "<img src=\"img/OIP".$order[$i].".jfif\">";
       }
         ?>
