81054b666bcbaf3241478ba0832cef1a7a98dde4
RubenHerB <rubenhb19@educastur.es>
2023-12-11 09:58:42 +0000

    11 dic 2023, 10:58

--- Ejercicios_php/Ejemplos/BBDD/Agenda/agenda.php
+++ Ejercicios_php/Ejemplos/BBDD/Agenda/agenda.php
@@ -3,10 +3,18 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Agenda</title>
+  <?php
+  if (isset($_POST["grabar"])){
 
+  }
+?>
 </head>
 <body>
     <h1>AGENDA</h1>
     <h2>Hola <?php session_start(); echo $_SESSION['usu']; ?></h2>
+    <form method="post" action="<
+    <?php 
+    fo
+    ?>
 </body>
 </html>
