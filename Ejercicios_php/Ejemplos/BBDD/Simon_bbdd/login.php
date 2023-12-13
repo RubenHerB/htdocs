8c9007aebfae779638623b0d848e4b3dfcd0629f
RubenHerB <rubenhb19@educastur.es>
2023-12-13 09:09:41 +0000

    13 dic 2023, 10:09

--- Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/login.php
+++ Ejercicios_php/Ejemplos/BBDD/Simon_bbdd/login.php
@@ -3,9 +3,23 @@ class login{
     function __construct(){
         
     }
-    function log() {
+
+
+    function log($n) {
+        if($n){
         $connection = new mysqli('localhost', 'user','user', 'bdsimon');
+    }else{
+        $connection = new mysqli('localhost', 'admin','admin', 'bdsimon');
+    }
     if ($connection->connect_error) die("Fatal Error");
     return $connection;}
    
+}
+
+class logininicial{
+    function log() {
+        $connection = new mysqli('localhost', 'login','', 'bdsimon');
+    if ($connection->connect_error) die("Fatal Error");
+    return $connection;
+}
 }
