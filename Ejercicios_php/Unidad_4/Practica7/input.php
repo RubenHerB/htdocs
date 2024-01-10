28c49394bf149e85a17d4ef284e89b27f9154e71
RubenHerB <rubenhb19@educastur.es>
2024-01-10 09:27:29 +0000

    10 ene 2024, 10:27

--- Ejercicios_php/Unidad_4/Practica7/input.php
+++ Ejercicios_php/Unidad_4/Practica7/input.php
@@ -14,9 +14,21 @@
         <h1>Introducir articulos</h1>
         <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             Descripcion: <input type="text" name="descripcion" required><br>
-            Precio: <input type="decimal" name="precio" required><br>
+            Precio: <input type="number" name="precio" required><br>
             Caracteristicas: <input type="text" name="caracteristicas"><br>
             <input type="submit" value="Introducir articulo">
         </form>
+        <?php
+        if(isset($_POST)){
+            $connection = new mysqli('localhost', 'administrador','administrador', 'ventas');
+            $query = "INSERT INTO articulos (descripcion,precio,caracteristicas) VALUES ('".$_POST["descripcion"]."',".$_POST["precio"].",'".$_POST["caracteristicas"]."')";
+            $result = $connection->query($query);
+            if (!$result){
+                die("Fatal Error");
+            }else{
+                echo "<br>El articulo se ha registrado exitosamente";
+            }
+        }
+        ?>
     </body>
 </html>
