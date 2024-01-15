abeec9d5b67f391fa931209a796b884cd99a4038
RubenHerB <rubenhb19@educastur.es>
2024-01-15 10:10:11 +0000

    15 ene 2024, 11:10

--- Ejercicios_php/Unidad_4/Practica6/inicio.php
+++ Ejercicios_php/Unidad_4/Practica6/inicio.php
@@ -15,5 +15,12 @@
     ?>
 
     <h1>Hola, <?php echo $_SESSION["name"]; ?></h1>
+    
+    <h2>Nuestros productos</h2>
+    <?php
+        include("login.php");
+        $log= new login();
+        $connection=$log->log_user();
+    ?>
     </body>
 </html>
