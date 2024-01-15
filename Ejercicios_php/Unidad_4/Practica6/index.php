b6b9463796e6947387f67957d87d2c272a70d453
RubenHerB <rubenhb19@educastur.es>
2024-01-15 09:05:09 +0000

    15 ene 2024, 10:05

--- Ejercicios_php/Unidad_4/Practica6/index.php
+++ Ejercicios_php/Unidad_4/Practica6/index.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <link rel="stylesheet" href="estilos.css">
+    <script type="text/javascript">
+        function aumentar(id) {
+            document.getElementById(id).value++;
+        }
+
+        function reducir(id) {
+            var n=document.getElementById(id).value;
+            if(n>0){
+                n--;
+            }
+        }
+    </script>
+</head>
+<body>
 <button class="btn" onclick="reducir()">-</button>
-<input type="number" min="0" >
+<input type="number" min="0" value="0" id="b1">
 <button class="btn" onclick="aumentar()">+</button>
+</body>
+</html>
