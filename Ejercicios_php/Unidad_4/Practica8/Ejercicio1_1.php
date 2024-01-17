b201bf0d1904def40438365141cda949c220c096
RubenHerB <rubenhb19@educastur.es>
2024-01-17 12:03:55 +0000

    17 ene 2024, 13:03

--- Ejercicios_php/Unidad_4/Practica8/Ejercicio1_1.php
+++ Ejercicios_php/Unidad_4/Practica8/Ejercicio1_1.php
@@ -6,5 +6,16 @@
     <title>Numero</title>
 </head>
 <body>
+<?php
+        session_start();
+        if(!isset($_SESSION)){
+            $_SESSION=["num"=>0];
+        }
+    ?>
     <h1>Subir y bajar numero</h1>
+    Haz clic en los botones para moficar el valor
+    <form method="post" action="Ejercicio1_2.php>">
+        <input type="submit" name="submit" value="-" /><?php echo $_SESSION["num"]; ?> <input type="submit" name="submit" value="+" />
+        <input type="submit" name="submit" value="Poner a cero" />
+    </form>
     
