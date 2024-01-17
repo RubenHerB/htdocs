9b0feb2bc4878b9851f64cc95b9449fca367e947
RubenHerB <rubenhb19@educastur.es>
2024-01-17 12:22:12 +0000

    17 ene 2024, 13:22

--- Ejercicios_php/Unidad_4/Practica8/Ejercicio2_1.php
+++ Ejercicios_php/Unidad_4/Practica8/Ejercicio2_1.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Numero</title>
     <style type="text/css">
+        .graph{display: inline-block;}
         .linea{height: 10px;background-color: black;}
         .circulo{height: 10px;width: 10px;background-color: red;border-radius: 5px;}
     </style>
@@ -19,9 +20,9 @@
     ?>
     <h1>Mover un punto de derecha a izquierda</h1>
     Haz clic en los botones para mover el punto:
-    <form method="post" action="Ejercicio1_2.php">
+    <form method="post" action="Ejercicio2_2.php">
         <input type="submit" name="submit" value="<-" /><input type="submit" name="submit" value="->" /><br>
-        <div><div class="linea" style="width:<?php echo $_SESSION['ancho']; ?>px"></div><div class="circulo"></div><div class="linea" style="width:<?php echo 590-$_SESSION['ancho']; ?>px"></div></div>
+        <div class="graph"><div class="linea" style="width:<?php echo $_SESSION['ancho']; ?>px"></div><div class="circulo"></div><div class="linea" style="width:<?php echo 590-$_SESSION['ancho']; ?>px"></div></div>
         <input type="submit" name="submit" value="volver al centro" />
     </form>
 </body>
