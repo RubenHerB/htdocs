c60364482433cd1059186b6853bf2c2468b96866
RubenHerB <rubenhb19@educastur.es>
2024-01-17 13:01:42 +0000

    17 ene 2024, 14:01

--- Ejercicios_php/Unidad_4/Practica8/Ejercicio4_1.php
+++ Ejercicios_php/Unidad_4/Practica8/Ejercicio4_1.php
@@ -5,25 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Numero</title>
     <style type="text/css">
-        .graph{display: flex;}
-        .area{height: 600px;width: 600px;border:1px solid black;margin-left: 10px;margin-top: 10px;}
-        .circulo{height: 20px;width: 20px;background-color: red;border-radius: 10px;position: absolute;}
+        .a{height: 10px; background-color: lightblue;}
+        .b{height: 10px; background-color: lightsalmon;}
     </style>
 </head>
 <body>
 <?php
-        session_name("UD4P5E3");
+        session_name("UD4P5E4");
         session_start();
         if($_SESSION==null){
-            $_SESSION=["x"=>300, "y"=>300];
+            $_SESSION=["a"=>300, "b"=>300];
         }
         var_dump($_SESSION);
     ?>
     <h1>Votar una opcion</h1>
     Haz clic en los botones para votar por una opcion:
     <form method="post" action="Ejercicio3_2.php">
-        <input type="submit" name="submit" value="←" />
-        <input type="submit" name="submit" value="↑" />
+        <input type="submit" name="submit" value="a" /><div class="a"></div><br
+        <input type="submit" name="submit" value="b" /><div class="b"></div><br>
         <input type="submit" name="submit" value="poner a cero" /><br>
         
     </form>
