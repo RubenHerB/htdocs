124296d8d7dd7f516aaf067fa63537beffb28f07
RubenHerB <rubenhb19@educastur.es>
2024-01-17 12:25:43 +0000

    17 ene 2024, 13:25

--- Ejercicios_php/Unidad_4/Practica8/Ejercicio2_2.php
+++ Ejercicios_php/Unidad_4/Practica8/Ejercicio2_2.php
@@ -3,10 +3,18 @@ session_name("UD4P5E2");
     session_start();
     switch ($_POST["submit"]) {
         case "<-":
+            if($_SESSION["ancho"]>0){
             $_SESSION["ancho"]-=20;
+        }else{
+            $_SESSION["ancho"]=600;
+        }
             break;
         case "->":
-            $_SESSION["ancho"]+=20;
+            if($_SESSION["ancho"]<600){
+                $_SESSION["ancho"]+=20;
+            }else{
+                $_SESSION["ancho"]=0;
+            }
             break;
         default:
         $_SESSION["ancho"]=295;
