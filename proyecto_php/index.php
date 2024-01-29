c5a598f8b3b36a851c81d3dc98a933c769ef3ebe
RubenHerB <rubenhb19@educastur.es>
2024-01-29 09:57:48 +0000

    29 ene 2024, 10:57

--- proyecto_php/index.php
+++ proyecto_php/index.php
@@ -3,14 +3,14 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Incidencias CIFP Aviles</title>
     <link rel="stylesheet" href="css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
   </head>
   <body data-bs-theme="light">
   <div class="container border border-secondary rounded" style="max-width: 574px;">
-              <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+              <ul class="nav nav-pills nav-fill mb-3" id="pills-tab" role="tablist">
                 <li class="nav-item text-center">
                   <a class="nav-link active btl" id="pills-home-tab" data-toggle="pill" href="index.php" role="tab" aria-controls="pills-home" aria-selected="true">Acceder</a>
                 </li>
