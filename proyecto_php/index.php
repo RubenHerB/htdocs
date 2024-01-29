55d1c1f9a973c33399e3a5c95482a61896a4c575
RubenHerB <rubenhb19@educastur.es>
2024-01-29 09:35:26 +0000

    29 ene 2024, 10:35

--- proyecto_php/index.php
+++ proyecto_php/index.php
@@ -6,7 +6,7 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-  <body data-bs-theme="dark">
+  <body data-bs-theme="light">
   <div class="container border border-secondary rounded">
   AAAAAAA
   </div>
