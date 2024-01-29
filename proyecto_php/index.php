df22284fab9be10b33060b4a72fd6ac1da980779
RubenHerB <rubenhb19@educastur.es>
2024-01-29 09:51:55 +0000

    29 ene 2024, 10:51

--- proyecto_php/index.php
+++ proyecto_php/index.php
@@ -9,8 +9,16 @@
     
   </head>
   <body data-bs-theme="light">
-  <div class="container-sm border border-secondary rounded">
-  AAAAAAA
+  <div class="container border border-secondary rounded" style="max-width: 574px;">
+              <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item text-center">
+                  <a class="nav-link active btl" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Login</a>
+                </li>
+                <li class="nav-item text-center">
+                  <a class="nav-link btr" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Signup</a>
+                </li>
+               
+              </ul>
   </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
