56cb8651e08e5f04b381c777b46437e45c42fd0f
RubenHerB <rubenhb19@educastur.es>
2024-01-29 11:44:03 +0000

    29 ene 2024, 12:44

--- proyecto_php/index.php
+++ proyecto_php/index.php
@@ -21,7 +21,7 @@
               </ul>
               <div class="mb-3">
               <label for="exampleFormControlInput1" class="form-label">Direccion de correo electronico</label>
-              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="ejemplo@direccion.com">
+              <input type="email" class="form-control is-valid" id="exampleFormControlInput1" placeholder="ejemplo@direccion.com">
               </div>
   </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
