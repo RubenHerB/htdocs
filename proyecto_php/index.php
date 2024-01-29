f4e3df2299d00be4e45123b953d09ee0b9b0daff
RubenHerB <rubenhb19@educastur.es>
2024-01-29 12:09:08 +0000

    29 ene 2024, 13:09

--- proyecto_php/index.php
+++ proyecto_php/index.php
@@ -10,7 +10,7 @@
   </head>
   <body data-bs-theme="dark">
   <div class="container border border-secondary rounded align-middle" style="max-width: 574px;margin-top: 150px">
-
+  <form>
               <!-- Botones nav para cambiar entre el modulo de acceso y el modulo de registro -->
               <ul class="nav nav-pills nav-fill mb-3" id="pills-tab" role="tablist">
                 <li class="nav-item text-center">
@@ -31,11 +31,8 @@
               <label for="exampleFormControlInput1" class="form-label">Contraseña</label>
               <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
               </div>
-              
-
-    
-  
 </div>
+</form>
   </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
