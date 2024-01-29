d52c35133e099ac9bade97df3dc717249fcda1d9
RubenHerB <rubenhb19@educastur.es>
2024-01-29 11:03:15 +0000

    29 ene 2024, 12:03

--- proyecto_php/index.php
+++ proyecto_php/index.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="img/logo.png">
   </head>
-  <body data-bs-theme="light">
+  <body data-bs-theme="dark">
   <div class="container border border-secondary rounded align-middle" style="max-width: 574px;margin-top: 150px">
               <ul class="nav nav-pills nav-fill mb-3" id="pills-tab" role="tablist">
                 <li class="nav-item text-center">
