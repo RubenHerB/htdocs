62837c2bcfcec50e25747dab808c256cbc7f647d
RubenHerB <rubenhb19@educastur.es>
2024-01-31 08:55:27 +0000

    31 ene 2024, 9:55

--- proyecto_php/portfolio/desechables/pruebaid.php
+++ proyecto_php/portfolio/desechables/pruebaid.php
@@ -3,17 +3,17 @@
 $connection = new mysqli('localhost', 'root', '', 'incidencias');
 if ($connection->connect_error) die("Fatal Error");
 $query = 
-"SELECT * AS fuente
+"SELECT IdAlumno AS fuente
 FROM alumno
 WHERE IdAlumno = 'luis@p.es'
 UNION
-SELECT * AS fuente
-FROM tabla2
+SELECT IdProfesor AS fuente
+FROM profesor
 WHERE IdProfesor = 'luis@p.es'
 UNION
-SELECT * AS fuente
-FROM tabla3
-WHERE id = 'luis@p.es';"
+SELECT IdTutor AS fuente
+FROM tutor
+WHERE IdTutor = 'luis@p.es';"
 ;
 $result = $connection->query($query);
  if (!$result) die("Fatal Error");
