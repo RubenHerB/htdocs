26f14bc32a28228df8f5a9cf675df91c2c717b9a
RubenHerB <rubenhb19@educastur.es>
2024-01-31 08:14:19 +0000

    31 ene 2024, 9:14

---
+++ proyecto_php/portfolio/login.php
@@ -0,0 +1,23 @@
+<?php
+class login{
+    function __construct(){
+        
+    }
+
+
+    function log($user) {
+        $usu="";
+        $pass="";
+        switch ($user){
+            case 0:
+                $usu="registrotutorlegal";
+            break;
+        }
+
+        $connection = new mysqli('localhost', $usu,$pass, 'bdsimon');
+    
+    if ($connection->connect_error) die("Fatal Error");
+    return $connection;
+    }
+
+}
