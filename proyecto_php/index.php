0939d6467d4bbf9b6f4f51324d98d8de12d1ed6e
RubenHerB <rubenhb19@educastur.es>
2024-02-01 11:32:12 +0000

    1 feb 2024, 12:32

--- proyecto_php/index.php
+++ proyecto_php/index.php
@@ -60,7 +60,7 @@
             header("Location: /portfolio/alumno.php");
           }else{
             session_id("T".$r['IdTutor']);
-            $_SESSION=["tipo"=>1,"id"=>$r['IdTutor'],"nombre"=>$r['Nombre'],"apellidos"=>$r['Apellidos'],"lastlogtime"=>$r['LastLog']];
+            $_SESSION=["tipo"=>1,"id"=>$r['IdTutor'],"nombre"=>$r['Nombre'],"apellidos"=>$r['Apellidos'],"lastlogtime"=>$r['LastLog'],"idAlumno"=>-1];
             if($r['Confirm']==-1){
             header("Location: /portfolio/tutor.php");
           }else{
