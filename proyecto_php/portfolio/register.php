08cd566ead5023ca5d2738e625edfb3afde04949
RubenHerB <rubenhb19@educastur.es>
2024-02-01 08:16:42 +0000

    1 feb 2024, 9:16

--- proyecto_php/portfolio/register.php
+++ proyecto_php/portfolio/register.php
@@ -25,7 +25,7 @@ $number    = preg_match('@[0-9]@', $password);
 $specialChars = preg_match('@[^\w]@', $password);
 
 if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
-    echo 'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
+    echo 'La contraseña debe tener al menos 8 caracteres de longitud, mayusculas, minusculas, numeros y caracteres especiales.';
 }else{
     echo 'Strong password.';
 }
