24a626099caecb1d12394bd925cf212db087bc46
RubenHerB <rubenhb19@educastur.es>
2024-02-01 10:02:26 +0000

    1 feb 2024, 11:02

--- proyecto_php/portfolio/dark.js
+++ proyecto_php/portfolio/dark.js
@@ -5,9 +5,8 @@ var theme = window.localStorage.getItem('data-bs-theme');
 if(theme) {document.getElementById('bd').setAttribute('dat-bs-theme', theme);}
 else{
   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    checkBox.checked=true;
-    document.getElementById('bd').setAttribute('data-bs-theme', 'dark');
     window.localStorage.setItem('data-bs-theme', 'dark');
+    theme="dark";
 }}
 checkBox.checked = theme == 'dark' ? true : false;
 if (theme == 'dark') document.getElementById('bd').setAttribute('data-bs-theme', 'dark');
