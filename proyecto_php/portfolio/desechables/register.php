9bc05c4728306e2e530f35be7a94ba40e99c62be
RubenHerB <rubenhb19@educastur.es>
2024-02-01 11:45:04 +0000

    1 feb 2024, 12:45

--- proyecto_php/portfolio/desechables/register.php
+++ proyecto_php/portfolio/desechables/register.php
@@ -7,8 +7,8 @@ if ($connection->connect_error) die("Fatal Error");
 // ;
 
 $query = 
-"INSERT INTO profesor (Nombre,Apellidos,Mail,Contra,Rol)
-VALUES ('juan','a','juan@p.es','".password_hash("juan",PASSWORD_DEFAULT)."',0);"
+"INSERT INTO profesor (Nombre,Apellidos,Mail,Contra,Rol,Departamento)
+VALUES ('ana','a','ana@p.es','".password_hash("ana",PASSWORD_DEFAULT)."',0,'Informatica');"
 ;
 
 $result = $connection->query($query);
