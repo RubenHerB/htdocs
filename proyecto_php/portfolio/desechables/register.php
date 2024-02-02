af611df8e550af76cfa858845ed8b57a7fdd16ad
RubenHerB <rubenhb19@educastur.es>
2024-02-02 09:31:49 +0000

    2 feb 2024, 10:31

--- proyecto_php/portfolio/desechables/register.php
+++ proyecto_php/portfolio/desechables/register.php
@@ -1,14 +1,13 @@
 <?php 
 $connection = new mysqli('localhost', 'root', '', 'incidencias');
 if ($connection->connect_error) die("Fatal Error");
-$query = 
-"INSERT INTO alumno (Nombre,Apellidos,Mail,Contra,Nacimiento)
-VALUES ('luis','p p','luis@p.es','".password_hash("luis",PASSWORD_DEFAULT)."',2002-01-14);";
-
 // $query = 
-// "INSERT INTO profesor (Nombre,Apellidos,Mail,Contra,Rol,Departamento)
-// VALUES ('ana','a','ana@p.es','".password_hash("ana",PASSWORD_DEFAULT)."',0,'Informatica');"
-// ;
+// "INSERT INTO alumno (Nombre,Apellidos,Mail,Contra,Nacimiento)
+// VALUES ('luis','p p','luis@p.es','".password_hash("luis",PASSWORD_DEFAULT)."',2002-01-14);";
+
+$query = 
+"INSERT INTO profesor (Nombre,Apellidos,Mail,Contra,Rol,Departamento)
+VALUES ('jose','a','jose@p.es','".password_hash("jose",PASSWORD_DEFAULT)."',3,'Informatica');";
 
 $result = $connection->query($query);
  if (!$result) die("Fatal Error");
