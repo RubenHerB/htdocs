a098ee58ae9ae384fbd56bbcef1568da09599041
RubenHerB <rubenhb19@educastur.es>
2024-02-02 08:46:25 +0000

    2 feb 2024, 9:46

--- proyecto_php/portfolio/profesorprin.php
+++ proyecto_php/portfolio/profesorprin.php
@@ -34,15 +34,15 @@
 <h1>Panel de control</h1>
 <br>
 <div class="card md-3" >
-  <div class="card-body">
-    <h5 class="card-title">Profesor</h5>
+<h5 class="card-header">Profesor</h5>
+    <div class="card-body">    
     <h6 class="card-subtitle mb-2 text-body-secondary">Inicia sesion en modo profesor</h6>
     <p class="card-text">Esta funcion te permite iniciar sesion para ver, registrar, editar o borrar tus incidencias de los modulos que impartes.</p>
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
     <input type="hidden" name="modo" value="1">
     <button type="button" class="btn btn-primary">Iniciar sesión</button>
     </form>
-  </div>
+    </div>
 </div>
     <?php 
     include "login.php";
@@ -54,8 +54,9 @@
         foreach($result as $row){
             echo <<<_END
             <div class="card" style="width: 18rem;">
+            <h5 class="card-header">Tutor</h5>
                 <div class="card-body">
-                    <h5 class="card-title">Tutor</h5>
+                    
                     <h6 class="card-subtitle mb-2 text-body-secondary">
             _END;
             echo $row["Codigo"].".".$row["Year"];
