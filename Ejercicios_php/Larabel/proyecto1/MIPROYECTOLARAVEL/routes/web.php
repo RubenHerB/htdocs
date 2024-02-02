b68e43fdeb4cf8b71dae64fa4410eaff63810a0f
RubenHerB <rubenhb19@educastur.es>
2024-02-02 12:37:52 +0000

    2 feb 2024, 13:37

--- Ejercicios_php/Larabel/proyecto1/MIPROYECTOLARAVEL/routes/web.php
+++ Ejercicios_php/Larabel/proyecto1/MIPROYECTOLARAVEL/routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/2',function () {
-    return view('welcome2');
+Route::get('/holamundo',function () {
+    return view('holamundo');
 });
