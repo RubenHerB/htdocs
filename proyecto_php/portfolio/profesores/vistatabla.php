b40199b577256ef893e80af12d614c516b4479c1
RubenHerB <rubenhb19@educastur.es>
2024-02-09 12:44:20 +0000

    9 feb 2024, 13:44

--- proyecto_php/portfolio/profesores/vistatabla.php
+++ proyecto_php/portfolio/profesores/vistatabla.php
@@ -1,25 +1,6 @@
 
 <table class="table table-striped">
-    <thead>
-    <tr>
-        <th>Fecha</th>
-        <th>Gravedad</th>
-        <th>Nombre</th>
-        <th>Apellidos</th>
-        <th>Curso</th>
-        <th>Año</th>
-        <th>Asignatura</th>
-        <th>Incidencia</th>
-        <th>Observaciones</th>
-        <?php
-        session_start();
-        if($rolnow==1){
-            echo "<th>Gestion</th>";
-        }
-        ?>
-    </tr>
-    </thead>
-    <tbody>
+    
 <?php
 $filtroextra=$_POST["filtro"];
 
@@ -40,6 +21,48 @@ AND IdProfesor.in LIKE '".$_SESSION['id']."' $filtroextra
 ";
     include "../login.php";
     $con=(new login)->log($_SESSION['rolnow']);
+    $result = $con->query($query);
+    if (!$result) die("Fatal Error");
+    if($result->num_rows!=0){
+        echo <<<_END
+        <thead>
+        <tr>
+        <th>Fecha</th>
+        <th>Gravedad</th>
+        <th>Nombre</th>
+        <th>Apellidos</th>
+        <th>Curso</th>
+        <th>Año</th>
+        <th>Asignatura</th>
+        <th>Incidencia</th>
+        <th>Observaciones</th>
+        _END;
+
+        session_start();
+        if($rolnow==1){
+            echo "<th>Gestion</th>";
+        }
+        echo "
+    </tr>
+    </thead>
+    <tbody>";
+    if($rolnow==1){
+        foreach($result as $row){
+            foreach($row as $r){
+                echo "<tr>$r</tr>";
+            }
+            echo "<button>
+            </button>";
+        }
+    }else{
+        foreach($result as $row){
+            foreach($row as $r){
+                echo "<tr>$r</tr>";
+            }
+        }
+        }
+
+    }
 
 
 ?>
