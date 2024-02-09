dfa6e46eefccd17da1474fa4c0dfd2b235acf664
RubenHerB <rubenhb19@educastur.es>
2024-02-09 08:04:25 +0000

    9 feb 2024, 9:04

--- proyecto_php/portfolio/profesores/vista1.php
+++ proyecto_php/portfolio/profesores/vista1.php
@@ -19,27 +19,9 @@
         if($_SESSION["tipo"]!=0){
             header("Location: redirect.php");
         }else{
-            var_dump("entro");
-    if (isset($_POST["modo"])){
-        $modo=intval($_POST["modo"]);
-        $_SESSION["rolnow"]=$modo;
-        var_dump($modo);
-        switch($modo){
-            case 1:
-                header("Location: profesores/vista1.php");
-                break;
-            case 2:
-                if(!$_SESSION["clase"]=$_POST["clase"]) die ("Error");                
-                header("Location: profesores/tutor.php");
-                break;
-            case 3:
-                header("Location: profesores/equipo.php");
-                break;
-            default:
-                die("Error");
-                break;  
-        }
-    }
+            if($_SESSION["rolnow"]!=1){
+                header("Location: ../redirect.php");
+            }
         }
 
     }
