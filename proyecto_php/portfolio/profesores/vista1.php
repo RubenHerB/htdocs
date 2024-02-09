621165d9f9a7c26d2995f77dcbe45dced6171f2b
RubenHerB <rubenhb19@educastur.es>
2024-02-09 07:56:16 +0000

    9 feb 2024, 8:56

--- proyecto_php/portfolio/profesores/vista1.php
+++ proyecto_php/portfolio/profesores/vista1.php
@@ -10,7 +10,7 @@
 </head>
 <body id="bd" data-bs-theme="light">
 <?php include "../dark.php"; ?>
-<div class="container-sm">100% wide until small breakpoint</div>
+<div class="container border border-secondary rounded align-middle" style="margin-top: 40px">aaaa</div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
  <script src=../dark.js></script>
