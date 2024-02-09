0656e5765b8ac4b956517b46dbf9b964245471b0
RubenHerB <rubenhb19@educastur.es>
2024-02-09 07:53:54 +0000

    9 feb 2024, 8:53

--- proyecto_php/portfolio/profesores/vista1.php
+++ proyecto_php/portfolio/profesores/vista1.php
@@ -4,15 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Incidencias CIFP Aviles</title>
-    <link rel="stylesheet" href="../css/dark-mode.css">
+    <link rel="stylesheet" href="../../css/dark-mode.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="icon" type="image/x-icon" href="../../img/logo.png">tent="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" type="image/x-icon" href="../../img/logo.png">
 </head>
 <body id="bd" data-bs-theme="light">
 <?php include "../dark.php"; ?>
 <div class="container-sm">100% wide until small breakpoint</div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+ <script src=../dark.js></script>
 <script type="text/javascript">
     if (window.XMLHttpRequest) { // Mozilla, Safari, ...
     hr = new XMLHttpRequest();
