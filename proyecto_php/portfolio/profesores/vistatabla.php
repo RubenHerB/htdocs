7bbd6be40e7913d3ad9031a58d23264cd71013a2
RubenHerB <rubenhb19@educastur.es>
2024-02-09 12:12:09 +0000

    9 feb 2024, 13:12

--- proyecto_php/portfolio/profesores/vistatabla.php
+++ proyecto_php/portfolio/profesores/vistatabla.php
@@ -1,5 +1,17 @@
+<table class="table">
+    <tr>
+        <th>Fecha</th>
+        <th>Gravedad</th>
+        <th>Nombre</th>
+        <th>Apellidos</th>
+        <th>Curso</th>
+        <th>Incidencia</th>
+        <th>Observaciones</th>
+    </tr>
 <?php
+$query="SELECT Nombre.al as Nombre, Apellidos.al as Apellidos, Nombre.cla as Curso Year.cla as Year  Fecha.in as Fecha, Observaciones.in as Observaciones FROM incidencias as in INNER JOIN alumno as al"
 
-$filtro=$_POST['filtro'];
-echo $filtro;
+
+
 ?>
+</table>
