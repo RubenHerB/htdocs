e28ea8372c3591db781a90af27c05f576f4cde4b
RubenHerB <rubenhb19@educastur.es>
2024-02-09 07:45:17 +0000

    9 feb 2024, 8:45

---
+++ proyecto_php/portfolio/profesores/vista1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script type="text/javascript">
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+    hr = new XMLHttpRequest();
+    http_request.overrideMimeType('text/xml');
+} else if (window.ActiveXObject) { // IE
+    hr = new ActiveXObject("Microsoft.XMLHTTP");
+}
+http_request.onreadystatechange = function(){
+    // procesar la respuesta
+};
+</script>
+</html>
