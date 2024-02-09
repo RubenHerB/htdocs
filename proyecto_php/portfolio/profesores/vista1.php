c82a6f1fa669d5f3abd731293ab2df95050bfb5d
RubenHerB <rubenhb19@educastur.es>
2024-02-09 08:59:07 +0000

    9 feb 2024, 9:59

--- proyecto_php/portfolio/profesores/vista1.php
+++ proyecto_php/portfolio/profesores/vista1.php
@@ -86,11 +86,12 @@ if($nr==0){
         <label class="form-check-label" for="flexCheckDefault">
         $dep - $nom
         </label></li>
+        </ul></div>
         _END;
     }
 }
 ?>
-</ul></div>
+
 </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
