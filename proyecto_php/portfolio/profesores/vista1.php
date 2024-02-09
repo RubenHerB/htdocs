1ba4803f9d66fc6ce299eeb2679fc8acd776564e
RubenHerB <rubenhb19@educastur.es>
2024-02-09 10:50:23 +0000

    9 feb 2024, 11:50

--- proyecto_php/portfolio/profesores/vista1.php
+++ proyecto_php/portfolio/profesores/vista1.php
@@ -104,19 +104,19 @@ if($nr==0){
 </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
- <script src=../dark.js></script>
+<script src=../dark.js></script>
 <script type="text/javascript">
 
 function filtro(){
-    document.getElementById("
+    var leve=document.getElementById("leve");
 }
 
     function ajax(){
     if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-    hr = new XMLHttpRequest();
+    var hr = new XMLHttpRequest();
     http_request.overrideMimeType('text/xml');
 } else if (window.ActiveXObject) { // IE
-    hr = new ActiveXObject("Microsoft.XMLHTTP");
+    var hr = new ActiveXObject("Microsoft.XMLHTTP");
 }
     }
     function load(){
@@ -126,6 +126,7 @@ hr.onreadystatechange = function(){
     }
 };
 hr.open("POST", "filtro="+filtro(),"vistatabla.php");
+hr.send();
 }
 
 
