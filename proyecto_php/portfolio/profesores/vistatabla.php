a5e3e2874f8a0ec2f28fd79ab17f5122594bde2f
RubenHerB <rubenhb19@educastur.es>
2024-02-09 12:18:27 +0000

    9 feb 2024, 13:18

--- proyecto_php/portfolio/profesores/vistatabla.php
+++ proyecto_php/portfolio/profesores/vistatabla.php
@@ -9,10 +9,17 @@
         <th>Curso</th>
         <th>Incidencia</th>
         <th>Observaciones</th>
+        <?php
+        session_start();
+        if($rolnow==1){
+            echo "<th>Gestion</th>";
+        }
+        ?>
     </tr>
     </thead>
     <tbody>
 <?php
+
 $query="SELECT Nombre.al as Nombre, Apellidos.al as Apellidos, Nombre.cla as Curso Year.cla as Year  Fecha.in as Fecha, Observaciones.in as Observaciones FROM incidencias as in INNER JOIN alumno as al"
 
 
