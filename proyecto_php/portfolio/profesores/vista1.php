610ca08a99970586587479c14acd7dc62209f032
RubenHerB <rubenhb19@educastur.es>
2024-02-09 08:22:16 +0000

    9 feb 2024, 9:22

--- proyecto_php/portfolio/profesores/vista1.php
+++ proyecto_php/portfolio/profesores/vista1.php
@@ -37,10 +37,14 @@ if($result->num_rows==0){
 
 }else{
     echo <<<_END
-    <div class="dropdown">        
+    <div class="dropdown">
     <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-        </button>        <ul class="dropdown-menu">
+    <svg fill="#000000" height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve"><g><g>
+    <polygon points="0,0 0,128 201.143,329.143 201.143,512 310.857,475.429 310.857,329.143 512,128 512,0"/>
+    </g></g></svg>
+    
+        </button>        
+        <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
