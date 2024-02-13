2956dd1bb21faf6e66271da66ca6de2fcf335bcf
RubenHerB <rubenhb19@educastur.es>
2024-02-13 20:49:51 +0000

    Feb 13, 2024, 9:49 PM

--- proyecto_php/portfolio/profesores/vistaupdate.php
+++ proyecto_php/portfolio/profesores/vistaupdate.php
@@ -102,53 +102,7 @@ ON al.IdAlumno=inci.IdAlumno
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src=../dark.js></script>
 <script type="text/javascript">
-
-function ajaxasig(idclase){
-    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-    hr = new XMLHttpRequest();
-    hr.overrideMimeType('text/xml');
-} else if (window.ActiveXObject) { // IE
-    hr = new ActiveXObject("Microsoft.XMLHTTP");
-}
-hr.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-        document.getElementById("asig").innerHTML=this.responseText;
-    }
-};
-hr.open("POST","selects/selectasig.php");
-hr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded');
-hr.send("id="+idclase);
-}
-
-
-
-
-function ajaxalum(idasig){
-    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-    hr = new XMLHttpRequest();
-    hr.overrideMimeType('text/xml');
-} else if (window.ActiveXObject) { // IE
-    hr = new ActiveXObject("Microsoft.XMLHTTP");
-}
-hr.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-        document.getElementById("alum").innerHTML=this.responseText;
-    }
-};
-hr.open("POST","selects/selectalum.php");
-hr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded');
-hr.send(idasig);
-}
-
-
-document.querySelector("select[name=clase]").addEventListener("change", function(){
-    console.log(document.getElementById("clase").value);
-    if(document.getElementById("clase").value!="Clase"){
-    ajaxalum(document.getElementById("clase").value);}else{
-        document.getElementById("alum").innerHTML="<select class=\"form-select form-select-sm\" id=\"alumsel\" aria-label=\"Small select example\" disabled><option selected>Alumno</option></select>";
-    } 
-});
-
+    
 function grabar(){
 if(document.getElementById("infra").value!="Infraccion"){
     if (window.XMLHttpRequest) { // Mozilla, Safari, ...
@@ -164,7 +118,7 @@ hr.onreadystatechange = function(){
 };
 hr.open("POST","selects/editar.php");
 hr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded');
-hr.send((document.getElementById("clase").value)+"&idalumno="+(document.getElementById("alumsel").value)+"&fecha="+(document.getElementById("tiempo").value)+'&tipo='+(document.getElementById("infra").value)+"&observaciones="+(document.getElementById("obs").value));
+hr.send("id=".$_POST['id']"&tipo='+(document.getElementById("infra").value)+"&observaciones="+(document.getElementById("obs").value));
 }else{
     document.getElementById("respuesta").innerHTML="<div class=\"alert alert-danger\" role=\"alert\">Completa todos los campos para poder editar la incidencia</div>"
 }
