27d11a4b201689aa35ad99b624eb179d8771b1cb
RubenHerB <rubenhb19@educastur.es>
2024-02-13 20:54:08 +0000

    Feb 13, 2024, 9:54 PM

---
+++ proyecto_php/portfolio/profesores/selects/editar.php
@@ -0,0 +1,25 @@
+<?php 
+// var_dump($_POST);
+session_start(); 
+    if(!isset($_SESSION)){
+        header("Location: ../index.php");
+        
+    }else{
+        if($_SESSION["tipo"]!=0){
+            header("Location: redirect.php");
+        }else{
+            if($_SESSION["rolnow"]!=1){
+                header("Location: ../redirect.php");
+            }
+        }
+
+    }
+
+$query="UPDATE incidencia  Observaciones='".$_POST['observaciones']."', TipoIncidencia=".$_POST['tipo']." where IdIncidencia LIKE ".$_POST['id'];
+var_dump($query);
+include "../../login.php";
+$con=(new login)->log($_SESSION['rolnow']);
+$result = $con->query($query);
+if (!$result) die("Fatal Error");
+echo "<div class=\"alert alert-success\" role=\"alert\">La incidencia ha sido editada</div>"
+?>
--- proyecto_php/portfolio/profesores/vistaupdate.php
+++ proyecto_php/portfolio/profesores/vistaupdate.php
@@ -61,7 +61,7 @@ ON al.IdAlumno=inci.IdAlumno
             echo $r['nom']." ".$r['ape'];
         $tipo=$r['tipo'];
         $obs=$r['obs'];
-        var_dump($tipo);
+        // var_dump($tipo);
     ?></option>
 </select>
 
@@ -102,8 +102,8 @@ ON al.IdAlumno=inci.IdAlumno
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src=../dark.js></script>
 <script type="text/javascript">
-    
-function grabar(){
+
+function editar(){
 if(document.getElementById("infra").value!="Infraccion"){
     if (window.XMLHttpRequest) { // Mozilla, Safari, ...
     hr = new XMLHttpRequest();
