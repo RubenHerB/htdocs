1d7b84a4a465a9a45a5841c0625a0cdf0d528f8c
RubenHerB <rubenhb19@educastur.es>
2024-02-13 19:10:03 +0000

    Feb 13, 2024, 8:10 PM

---
+++ proyecto_php/portfolio/profesores/vistaestadisticas2.php
@@ -0,0 +1,87 @@
+
+<table class="table table-striped">
+    
+<?php
+session_start();
+$filtroextra=$_POST["filtro"];
+$prof="";
+if($_SESSION["rolnow"]==2){
+        $prof="WHERE cla.IdTutor LIKE '".$_SESSION['id']."'";
+    }
+    if($filtroextra!=""){
+        if($prof!=""){
+            $filtroextra = " AND ".$filtroextra;
+        }else{
+            $filtroextra = " WHERE ".$filtroextra;
+        }
+    }
+        $filtroextra=$prof.$filtroextra;
+
+var_dump($filtroextra);
+$query="SELECT MAX(contador) AS maximo_contador
+FROM (
+    SELECT COUNT(inc.*) AS contador
+    FROM incidencia as inc
+
+    Inner JOIN clases as cl
+    on in.IdClase = cl.IdClase
+    GROUP BY cl.Seccion
+    
+) AS conteos";
+
+
+include "../login.php";
+$con=(new login)->log($_SESSION['rolnow']);
+$result = $con->query($query);
+$result->data_seek(0);
+$maxin=$result->fetch_assoc()['maximo_contador'];
+
+$query="SELECT cla.Nombre as clase,cla.Year as year,cla.Tipo as tipo , cla.Seccion as dep, count(inci.IdIncidencia) as cuenta,  prof.Nombre as nombre, prof.Apellidos as apellidos 
+FROM incidencia as inci 
+
+INNER JOIN clases as cla 
+ON inci.IdClase=cla.IdClase
+
+INNER JOIN profesor as prof 
+ON prof.IdProfesor LIKE cla.IdTutor 
+
+INNER JOIN incidenciasbase as incibas 
+ON incibas.IdIncidenciaBase = inci.TipoIncidencia 
+
+INNER JOIN `alumno-clase` as alcl
+ON cla.IdClase = alcl.IdClase
+
+$filtroextra
+
+GROUP BY cla.Seccion
+ORDER BY cuenta  ".$_POST['orden'];
+var_dump($query);
+    
+    $result = $con->query($query);
+    if (!$result) die("Fatal Error");
+        // var_dump($query);
+        // var_dump($_SESSION);
+        echo <<<_END
+        <thead>
+        <tr>
+        <th>Departamento</th>
+        <th>Incidencias</th>
+        <th>Grafica</th>
+        </tr>
+        </thead>
+        <tbody>
+    _END;
+    
+      foreach($result as $row){
+        echo "<tr>";
+        echo "<td>".$row['clase']." ".$row['year']."º ".$row['tipo']."</td>";
+        echo "<td>".$row['dep']."</td>";
+        echo "<td>".$row['cuenta']."</td>";
+        echo "<td>".$row['nombre']." ".$row['apellidos']."</td>";
+        echo "<td><div style=\"height:20px;background-color:red;width:".(200*($row['cuenta'])/$maxin)."px\"></div></td>";
+      }
+
+
+?>
+    </tbody>
+</table>
