d9a255a600533f4832b0347b7a214b799a7d4827
RubenHerB <rubenhb19@educastur.es>
2024-02-13 10:45:25 +0000

    Feb 13, 2024, 11:45 AM

--- proyecto_php/portfolio/profesores/vista1.php
+++ proyecto_php/portfolio/profesores/vista1.php
@@ -52,7 +52,7 @@ if($nr==0){
     _END;
 }else{
     echo <<<_END
-    <div class="dropdown">
+    <div class="d-flex justify-content-between"><div class="dropdown">
     <button class="btn btn-primary dropdown-toggle tipo" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     <svg fill="#000000" height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve"><g><g>
     <polygon points="0,0 0,128 201.143,329.143 201.143,512 310.857,475.429 310.857,329.143 512,128 512,0"/>
@@ -99,7 +99,7 @@ if($nr==0){
         <li>
         <button class="btn btn-primary type="button" onclick="ajaxtabla()">Filtrar</button>
         </li>
-        </ul></div>
+        </ul></div> <button class="btn btn-primary type="button" onclick="ajaxtabla()">Filtrar</button></div>
         _END;
 
 }
