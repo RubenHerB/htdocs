09159dbcc04f45018e9d17e349bd9a98411afb90
RubenHerB <rubenhb19@educastur.es>
2024-02-13 18:24:34 +0000

    Feb 13, 2024, 7:24 PM

--- proyecto_php/portfolio/profesores/vistaestadisticas.php
+++ proyecto_php/portfolio/profesores/vistaestadisticas.php
@@ -6,7 +6,7 @@ session_start();
 $filtroextra=$_POST["filtro"];
 
 
-$query="SELECT cla.Nombre as clase,cla.Year as year,cla.Tipo as tipo , cla.Seccion, count(inci.IdIncidencia) as cuenta, prof.Nombre as nombre, prof.Apellidos as apellidos 
+$query="SELECT cla.Nombre as clase,cla.Year as year,cla.Tipo as tipo , cla.Seccion, count(inci.IdIncidencia) as cuenta, max(count(inci.IdIncidencia)) as maximo, prof.Nombre as nombre, prof.Apellidos as apellidos 
 FROM incidencia as inci 
 
 INNER JOIN clases as cla 
@@ -31,42 +31,15 @@ $filtroextra";
         <th>Incidencias</th>
         <th>Tutor</th>
         <th>Grafica</th>
-       
         </tr>
         </thead>
         <tbody>
     _END;
-    if($_SESSION['rolnow']==1){
-        foreach($result as $row){
-            echo "<tr    id=\"".$row['id']."\">";
-            foreach($row as $d=>$r){
-                if($d!="Observaciones"){
-                echo "<td>$r</td>";
-            }else{
-                echo "<td class=\"overflow-auto\" style=\"max-width:20vw\">$r</td>";
-            }
-        }
-            echo "<td><button onclick=\"editar(".$row['id'].")\">Editar
-            </button><button onclick=\"borrar(".$row['id'].")\">Borrar
-            </button></td></tr>";
-    }
-    }else{
-        foreach($result as $row){
-            echo "<tr>";
-            foreach($row as $r){
-                echo "<td>$r</td>";
-            }
-            echo "</tr>";
-        }
-        }
-
-    }else{
-        echo <<<_END
-        <div class="alert alert-secondary" role="alert" style="margin-top:16px">
-    Sin incidencias
-    </div>
-    _END;
-    }
+    
+      foreach($result as $row){
+        echo "<tr>";
+        echo "<td>".$row
+      }
 
 
 ?>
