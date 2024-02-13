e54ff4a8f92a6bdbfaa154e5a170b2969b10ce56
RubenHerB <rubenhb19@educastur.es>
2024-02-13 20:08:23 +0000

    Feb 13, 2024, 9:08 PM

--- proyecto_php/portfolio/profesores/selects/grabar.php
+++ proyecto_php/portfolio/profesores/selects/grabar.php
@@ -1,6 +1,21 @@
 <?php 
 var_dump($_POST);
-$day1 = strtotime($_POST["date1"]);
-$day1 = date('Y-m-d H:i:s', $day1)
+session_start(); 
+    if(!isset($_SESSION)){
+        header("Location: ../index.php");
+        
+    }else{
+        if($_SESSION["tipo"]!=0){
+            header("Location: redirect.php");
+        }else{
+            if($_SESSION["rolnow"]!=1){
+                header("Location: ../redirect.php");
+            }
+        }
 
+    }
+$day = strtotime($_POST["fecha"]);
+$day = date('Y-m-d H:i:s', $day)
+$query="INSERT INTO incidencia (IdClase, Fecha, IdProfesor, Observaciones, TipoIncidencia,IdAlumno,Asignatura) VALUES ('$day1',
+(".$_POST['idc'].", $day ,".$_SESSION['id'].
 ?>
