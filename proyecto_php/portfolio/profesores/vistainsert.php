f4e69e71936708fbd3526074595a0581a1f21bc1
RubenHerB <rubenhb19@educastur.es>
2024-02-13 13:13:34 +0000

    Feb 13, 2024, 2:13 PM

--- proyecto_php/portfolio/profesores/vistainsert.php
+++ proyecto_php/portfolio/profesores/vistainsert.php
@@ -130,7 +130,7 @@ hr.send("id="+idasig);
 document.getElementById("clase").addEventListener("change", (event) => {
     console.log(document.getElementById("asigsel").value);
     if(document.getElementById("asigsel").value!="Asignatura"){
-    ajaxalum(document.getElementById("alum").value);}else{
+    ajaxalum(document.getElementById("asigsel").value);}else{
         document.getElementById("alum").innerHTML="<select class=\"form-select form-select-sm\" id=\"alumsel\" aria-label=\"Small select example\" disabled><option selected>Alumno</option></select>";
     } 
 });
