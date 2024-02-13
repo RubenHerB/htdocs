99b3d33d8493e2d7f300f255cf07e1d3e62f9463
RubenHerB <rubenhb19@educastur.es>
2024-02-13 11:09:37 +0000

    Feb 13, 2024, 12:09 PM

--- proyecto_php/portfolio/profesores/vistainsert.php
+++ proyecto_php/portfolio/profesores/vistainsert.php
@@ -48,13 +48,10 @@
   <option value="2">Two</option>
   <option value="3">Three</option>
 </select>
-<select class="form-select form-select-sm" aria-label="Small select example" disabled>
+<select class="form-select form-select-sm" id="asig" aria-label="Small select example" disabled>
   <option selected>Asignatura</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
 </select>
-<select class="form-select form-select-sm" aria-label="Small select example" disabled>
+<select class="form-select form-select-sm" id="alum" aria-label="Small select example" disabled>
   <option selected>Alumno</option>
   <option value="1">One</option>
   <option value="2">Two</option>
