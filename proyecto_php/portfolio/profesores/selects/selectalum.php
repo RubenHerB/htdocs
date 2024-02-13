0705993ec11ed8a71e3e610d57278917dcf99d1a
RubenHerB <rubenhb19@educastur.es>
2024-02-13 13:07:19 +0000

    Feb 13, 2024, 2:07 PM

---
+++ proyecto_php/portfolio/profesores/selects/selectalum.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+$query="SELECT IdAsignatura, Nombre FROM asignaturas
+    WHERE IdProfesor=".$_SESSION['id']." AND IdClase LIKE ".$_POST['id']." GROUP BY Nombre";
+    
+        include "../../login.php";
+        $con=(new login)->log($_SESSION['rolnow']);
+        $result = $con->query($query);
+        if (!$result) die("Fatal Error");
+
+?>
+<select class="form-select form-select-sm" id="alumsel" aria-label="Small select example">
+  <option selected>Asignatura</option>
+<?php
+        foreach ($result as $row){
+            echo "<option value=\"".$row['IdAsignatura']."\">".$row['Nombre']."</option>";
+        }
+?>
+
+        </select>
+
