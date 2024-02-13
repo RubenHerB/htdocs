e29ec651184db2d6111f0f8d9e119fefe070364f
RubenHerB <rubenhb19@educastur.es>
2024-02-13 14:57:14 +0000

    Feb 13, 2024, 3:57 PM

--- proyecto_php/portfolio/profesores/vistainsert.php
+++ proyecto_php/portfolio/profesores/vistainsert.php
@@ -96,6 +96,7 @@
 </div>
 <br>
 </form>
+<button type="button" onclick="grabar()" class="btn btn-primary">Panel de control</button>
 </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
