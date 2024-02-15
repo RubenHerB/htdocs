c7ef6cd0a9a35c8f417930e95b4d5bb5e956421c
RubenHerB <rubenhb19@educastur.es>
2024-02-15 20:28:32 +0000

    Feb 15, 2024, 9:28 PM

--- examenes/export.php
+++ examenes/export.php
@@ -3,6 +3,6 @@ $connection = new mysqli('localhost', "root", "", 'examen');
 if ($connection->connect_error) die("Fatal Error");
 $query="SELECT * FROM preguntas";
 $result = $connection->query($query);
-$aleatorio=range(0,($result->num_row)-1,1);
+$aleatorio=range(0,($result->num_rows)-1,1);
 var_dump($aleatorio);
 ?>
