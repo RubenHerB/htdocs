f445d3f9e58ff5919711d2b01a54c84a93fd4eda
RubenHerB <rubenhb19@educastur.es>
2024-02-15 20:57:24 +0000

    Feb 15, 2024, 9:57 PM

---
+++ proyecto_php/portfolio/profesores/selects/comprobar.php
@@ -0,0 +1,6 @@
+<?php
+$connection = new mysqli('localhost', "root", "", 'examen');    
+if ($connection->connect_error) die("Fatal Error");
+$query="SELECT * FROM preguntas";
+$result = $connection->query($query);
+?>
