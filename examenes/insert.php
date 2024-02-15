a38ef41b21c59a205bae6e9d87e9e360c047b726
RubenHerB <rubenhb19@educastur.es>
2024-02-15 20:09:07 +0000

    Feb 15, 2024, 9:09 PM

--- examenes/insert.php
+++ examenes/insert.php
@@ -1,13 +1,13 @@
 <?php
 var_dump($_POST);
 $pregunta=$_POST["pregunta"];
-$respuestac=$_POST["respuestac"];
-$respuestai1=$_POST["respuestai1"];
-$respuestai2=$_POST["respuestai2"];
-$respuestai3=$_POST["respuestai3"];
+$respuestac=$_POST["respuestacorrecta"];
+$respuestai1=$_POST["respuestaincorrecta1"];
+$respuestai2=$_POST["respuestaincorrecta2"];
+$respuestai3=$_POST["respuestaincorrecta3"];
 
 $connection = new mysqli('localhost', "root", "", 'examen');    
 if ($connection->connect_error) die("Fatal Error");
-$query="INSERT INTO preguntas (Pregunta,Respuesta1,Respuesta2,Respuesta3,Respuesta4) VALUES ('$pregunta', '$respuestac', '$resppuestai1', '$resppuestai2', '$respuestai3')";
+$query="INSERT INTO preguntas (Pregunta,Respuesta1,Respuesta2,Respuesta3,Respuesta4) VALUES ('$pregunta', '$respuestac', '$respuestai1', '$respuestai2', '$respuestai3')";
 $result = $connection->query($query);
 ?>
