3be2de41a62f03dbc1d21c2e49a2af0645d9210d
RubenHerB <rubenhb19@educastur.es>
2024-02-15 20:05:53 +0000

    Feb 15, 2024, 9:05 PM

--- examenes/insert.php
+++ examenes/insert.php
@@ -1,3 +1,8 @@
 <?php
 var_dump($_POST);
+$pregunta=$_POST["pregunta"];
+$respuestac=$_POST["respuestac"];
+$respuestai1=$_POST["respuestai1"];
+$respuestai2=$_POST["respuestai2"];
+$respuestai3=$_POST["respuestai3"];
 ?>
