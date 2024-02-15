7caddf81d7993f776e5e5940d8579a2594a698df
RubenHerB <rubenhb19@educastur.es>
2024-02-15 20:07:02 +0000

    Feb 15, 2024, 9:07 PM

--- examenes/insert.php
+++ examenes/insert.php
@@ -5,4 +5,9 @@ $respuestac=$_POST["respuestac"];
 $respuestai1=$_POST["respuestai1"];
 $respuestai2=$_POST["respuestai2"];
 $respuestai3=$_POST["respuestai3"];
+
+$connection = new mysqli('localhost', "root",, 'examenes');    
+if ($connection->connect_error) die("Fatal Error");
+$query
+$result = $connection->query($query);
 ?>
