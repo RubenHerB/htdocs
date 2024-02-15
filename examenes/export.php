265f5070673b3881c3282c5ef108be8cddbfc052
RubenHerB <rubenhb19@educastur.es>
2024-02-15 20:31:25 +0000

    Feb 15, 2024, 9:31 PM

--- examenes/export.php
+++ examenes/export.php
@@ -4,7 +4,8 @@ if ($connection->connect_error) die("Fatal Error");
 $query="SELECT * FROM preguntas";
 $result = $connection->query($query);
 shuffle($result);
-for($i=0;$i<10$i++){
-    var_dump($result[$i]+"<br>");
+for($i=0;$i<10;$i++){
+    $result->data_seek($i);
+    var_dump($result+"<br>");
 }
 ?>
