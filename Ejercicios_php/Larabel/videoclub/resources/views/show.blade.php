99bb732614115ac82bb652263dbd3a9f934f241f
RubenHerB <rubenhb19@educastur.es>
2024-02-23 12:49:15 +0000

    23 feb 2024, 13:49

--- Ejercicios_php/Larabel/videoclub/resources/views/show.blade.php
+++ Ejercicios_php/Larabel/videoclub/resources/views/show.blade.php
@@ -8,6 +8,15 @@
 <h2 style="min-height:45px;margin:5px 0 10px 0">
 {{$pelicula['title']}}
 </h2>
+<p>Año: {{$pelicula['year']}}</p>
+<p>Director: {{$pelicula['director']}}</p>
+<p>{{$pelicula['synopsis']}}</p>
+
+			'year' => '1999',
+			'director' => 'David Fincher',
+			'poster' => 'http://ia.media-imdb.com/images/M/MV5BMjIwNTYzMzE1M15BMl5BanBnXkFtZTcwOTE5Mzg3OA@@._V1_SX214_AL_.jpg',
+			'rented' => true,
+			'synopsis'
 </div>
 </div>
 @endsection
