e9778c8f38cd90c5507e799ce78bbaeea73afa1d
RubenHerB <rubenhb19@educastur.es>
2024-02-23 12:47:18 +0000

    23 feb 2024, 13:47

--- Ejercicios_php/Larabel/videoclub/resources/views/show.blade.php
+++ Ejercicios_php/Larabel/videoclub/resources/views/show.blade.php
@@ -2,10 +2,12 @@
 @section('contenido')
 <div class="row">
 <div class="col-sm-4">
-{{-- TODO: Imagen de la película --}}
+<img src="{{$pelicula['poster']}}" style="height:200px"/>
 </div>
 <div class="col-sm-8">
-{{-- TODO: Datos de la película --}}
+<h2 style="min-height:45px;margin:5px 0 10px 0">
+{{$pelicula['title']}}
+</h2>
 </div>
 </div>
 @endsection
