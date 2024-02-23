be3c02271ff2874279e735f20ef3f4a76888b647
RubenHerB <rubenhb19@educastur.es>
2024-02-23 12:53:55 +0000

    23 feb 2024, 13:53

--- Ejercicios_php/Larabel/videoclub/resources/views/show.blade.php
+++ Ejercicios_php/Larabel/videoclub/resources/views/show.blade.php
@@ -2,7 +2,7 @@
 @section('contenido')
 <div class="row">
 <div class="col-sm-4">
-<img src="{{$pelicula['poster']}}" style="height:200px"/>
+<img src="{{$pelicula['poster']}}" style="height:500px"/>
 </div>
 <div class="col-sm-8">
 <h2 style="min-height:45px;margin:5px 0 10px 0">
