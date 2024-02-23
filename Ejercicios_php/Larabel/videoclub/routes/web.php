dcfd49b2d3488d6737402cb5af0b679a94fbca41
RubenHerB <rubenhb19@educastur.es>
2024-02-23 12:30:11 +0000

    23 feb 2024, 13:30

--- Ejercicios_php/Larabel/videoclub/routes/web.php
+++ Ejercicios_php/Larabel/videoclub/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Operaciones;
+use App\Http\Controllers\CatalogControler;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,29 +14,9 @@ use App\Http\Controllers\Operaciones;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('login', function () {
-    return 'Login usuario';
-});
-Route::get('logout', function () {
-    return 'Logout usuario';
-});
-Route::get('catalog', function () {
-    return 'Listado películas';
-});
-Route::get('catalog/show/{id?}', function ($id=null) {
-    return 'Vista detalle película '.$id;
-})->where('id', '[0-9]+');
-Route::get('catalog/create', function () {
-    return 'Añadir pelicula';
-});
-Route::get('catalog/edit/{id?}', function ($id=null) {
-    return 'Modificar película '.$id    ;
-})->where('id', '[0-9]+');
+Route::get('/', [CatalogControler::class, 'getHome']);
+Route::get('catalog', [CatalogControler::class, 'getIndex']);
 
-Route::get('Suma/{a}/{b}', [operaciones::class, 'suma']);
-Route::get('Resta/{a}/{b}', [operaciones::class, 'resta']);
-Route::get('Div/{a}/{b}', [operaciones::class, 'div']);
-Route::get('Mullti/{a}/{b}', [operaciones::class, 'multi']);
+Route::get('user/{id}', [CatalogControler::class, 'showProfile']);
+
+
