366a8816e330888857d623130e3888e013a67c1d
RubenHerB <rubenhb19@educastur.es>
2024-02-23 12:06:08 +0000

    23 feb 2024, 13:06

--- Ejercicios_php/Larabel/videoclub/app/Http/Controllers/Operaciones.php
+++ Ejercicios_php/Larabel/videoclub/app/Http/Controllers/Operaciones.php
@@ -8,22 +8,23 @@ class operaciones extends Controller
 {
 public function suma( $a, $b)
 {
-    return view('Suma',["suma"=>($a+$b)]);
+    return view('Suma',['suma'=>($a+$b)]);
 }
 
 public function resta(Request $request,  $a, $b)
 {
-echo $a-$b;
+    return view('Suma',['suma'=>($a-$b)]);
 }
 
 public function div(Request $request,  $a, $b)
 {
-echo $a/$b;
+    return view('Suma',['suma'=>($a/$b)]);
 }
 
 public function mult(Request $request,  $a, $b)
 {
-echo $a*$b;
+    return view('Suma',['suma'=>($a*$b)]);
+
 }
 
 }
