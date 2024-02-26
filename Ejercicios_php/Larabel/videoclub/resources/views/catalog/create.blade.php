99d000c27622e70a38685231e573fe7e1d945228
RubenHerB <rubenhb19@educastur.es>
2024-02-26 09:18:37 +0000

    26 feb 2024, 10:18

--- Ejercicios_php/Larabel/videoclub/resources/views/catalog/create.blade.php
+++ Ejercicios_php/Larabel/videoclub/resources/views/catalog/create.blade.php
@@ -4,6 +4,11 @@
 <h2>Añadir peliculas</h2>
 
 <div class="col-sm-8">
-
+    <form>
+    <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Titulo de la pelicula</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" >
+</div>
+    </form>
 </div>
 @endsection
