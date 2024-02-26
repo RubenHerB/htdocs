12575437c6027aac4ab93f8c949d04c489dc9d89
RubenHerB <rubenhb19@educastur.es>
2024-02-26 09:23:59 +0000

    26 feb 2024, 10:23

--- Ejercicios_php/Larabel/videoclub/resources/views/catalog/create.blade.php
+++ Ejercicios_php/Larabel/videoclub/resources/views/catalog/create.blade.php
@@ -8,22 +8,22 @@
     <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Titulo de la pelicula</label>
   <input type="text" class="form-control" name="titulo">
-
+  <br>
   <label for="exampleFormControlInput1" class="form-label">Año de la pelicula</label>
   <input type="number" class="form-control" name="year">
-
+  <br>
   <label for="exampleFormControlInput1" class="form-label">Director de la pelicula</label>
   <input type="text" class="form-control" name="director">
-
+  <br>
   <label for="exampleFormControlInput1" class="form-label">Url del poster de la pelicula</label>
   <input type="text" class="form-control" name="poster">
 </div>
-
+<br>
 <div class="mb-3">
   <label for="exampleFormControlTextarea1" class="form-label">Sypnosis</label>
   <textarea class="form-control" name="sypnosis" rows="3"></textarea>
 </div>
-
+<br>
 <div class="col-auto">
     <button type="submit" class="btn btn-primary mb-3">Añadir pelicula</button>
   </div>
