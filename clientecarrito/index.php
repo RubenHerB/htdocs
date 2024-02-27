3de6b79ce111bdd280299710aaa34b7933b23814
RubenHerB <rubenhb19@educastur.es>
2024-02-27 07:46:57 +0000

    27 feb 2024, 8:46

--- clientecarrito/index.php
+++ clientecarrito/index.php
@@ -5,8 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .badge:after{
+content:attr(value);
+font-size:12px;
+background: red;
+border-radius:50%;
+padding:3px;
+position:relative;
+left:-8px;
+top:-10px;
+opacity:0.9;
+}
+    </style>
 </head>
 <body>
-<i class="fa badge" style="font-size:24px" value=5>&#xf07a;</i>
+<i class="fa badge" style="font-size:24px" value=0>&#xf07a;</i>
 </body>
 </html>
