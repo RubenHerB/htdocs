7aabc0175b5dc109bd8b10f2e749cfdb41607ed4
RubenHerB <rubenhb19@educastur.es>
2024-03-04 10:00:05 +0000

    4 mar 2024, 11:00

--- Ejercicios_php/Larabel/videoclub/app/Http/Controllers/CatalogControler.php
+++ Ejercicios_php/Larabel/videoclub/app/Http/Controllers/CatalogControler.php
@@ -8,45 +8,43 @@ use App\Http\Middleware\VerifyCsrfToken;
 class CatalogControler extends Controller
 {
     public function getHome(){
-    return redirect()->action([CatalogControler::class, 'getIndex']);
+        return redirect()->action([CatalogControler::class, 'getIndex']);
     }
 
     public function getIndex(){
         $arrayPeliculas= Movie::all();
         return view("catalog.index",compact('arrayPeliculas'));
-        }
+    }
         
 
     public function getShow($id)
-{
-    $arrayPeliculas= Movie::find($id);
-return view('catalog.show',array('pelicula'=>$arrayPeliculas));
-}
-
-public function getCreate(){
-    return view('catalog.create');
-}
-
-public function getEdit($id){
-    $arrayPeliculas= Movie::find($id);
-    return view('catalog.edit',array('pelicula'=>$arrayPeliculas));
-    
-}
-
-public function store(Request $request){
-    if($request->post('title')!="" && $request->post('year')!="" && $request->post('director')!=""
-    && $request->post('poster')!="" && $request->post('synopsis')!=""){
-    $peli = new Movie;
-    $peli->title =$request->post('title');
-    $peli->year =$request->post('year');
-    $peli->director =$request->post('director');
-    $peli->poster =$request->post('poster');
-    $peli->synopsis =$request->post('synopsis');
-
-    $peli->save();
+    {
+        $arrayPeliculas= Movie::find($id);
+        return view('catalog.show',array('pelicula'=>$arrayPeliculas));
     }
+
+    public function getCreate(){
+        return view('catalog.create');
+    }
+
+    public function getEdit($id){
+        $arrayPeliculas= Movie::find($id);
+        return view('catalog.edit',array('pelicula'=>$arrayPeliculas));
+    }
+
+    public function store(Request $request){
+        if($request->post('title')!="" && $request->post('year')!="" && $request->post('director')!=""
+        && $request->post('poster')!="" && $request->post('synopsis')!=""){
+            $peli = new Movie;
+            $peli->title =$request->post('title');
+            $peli->year =$request->post('year');
+            $peli->director =$request->post('director');
+            $peli->poster =$request->post('poster');
+            $peli->synopsis =$request->post('synopsis');
+            $peli->save();
+        }
     return redirect()->route('catalog.store')->with('success','La pelicula se ha grabado');
-}
+    }
 
 public function actualizar(Request $request,$id){
     if($request->post('title')!="" && $request->post('year')!="" && $request->post('director')!=""
