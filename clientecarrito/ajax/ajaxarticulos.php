f77ce0bf456c7e6f102e7d9973358b59c32fb206
RubenHerB <rubenhb19@educastur.es>
2024-03-05 08:31:09 +0000

    5 mar 2024, 9:31

--- clientecarrito/ajax/ajaxarticulos.php
+++ clientecarrito/ajax/ajaxarticulos.php
@@ -18,5 +18,17 @@ $result = $connection->query($query);
 if (!$result){
     die("Fatal Error");
 }
+foreach($result as $row){
+    echo "<div class=\"row\">
+    <div class=\"col-md-7\">
+        <img class=\"img-fluid rounded mb-3 mb-md-0\" src=\"img/".$row['id'].".png\" alt=\"\">
+    </div>
+    <div class=\"col-md-5\">
+    <h3>Project One</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+    <a class=\"btn btn-primary\" href=\"#\">View Project</a>
+    </div>
+  </div>";
+}
 }
 
