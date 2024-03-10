9d1db7b2d91c6824230fa94a5112daf658aa5703
RubenHerB <rubenhb19@educastur.es>
2024-03-10 11:19:05 +0000

    Mar 10, 2024, 12:19 PM

---
+++ clientecarrito/prueba.php
@@ -0,0 +1,14 @@
+<?php
+
+    
+    $query="SELECT * FROM `articulos`";
+    $connection = new mysqli('localhost', 'root','', 'carritocliente');
+    if ($connection->connect_error){
+        die("Fatal Error");
+    }
+$result = $connection->query($query);
+if (!$result){
+    die("Fatal Error");
+}
+echo var_dump($result);
+
