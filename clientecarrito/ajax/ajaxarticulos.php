6adc070222cbc538f4d8cbf736e55063b4ecb81f
RubenHerB <rubenhb19@educastur.es>
2024-03-10 10:05:56 +0000

    Mar 10, 2024, 11:05 AM

--- clientecarrito/ajax/ajaxarticulos.php
+++ clientecarrito/ajax/ajaxarticulos.php
@@ -17,38 +17,6 @@ $result = $connection->query($query);
 if (!$result){
     die("Fatal Error");
 }
-foreach($result as $row){
-    echo "<div class=\"row\">
-    <div class=\"col-md-4\" id=\"pop\">
-    <button type=\"button\" class=\"btn\" data-bs-toggle=\"modal\" data-bs-target=\"#modal".$row['codArticulo']."\">
-        <img class=\"img-fluid rounded mb-3 mb-md-0\" src=\"img/".$row['codArticulo'].".png\" alt=\"\" style=\"max-height:100px\">
-        </button>
-    </div>
-    <div class=\"col-md-8\">
-    <h3>".$row['nombre']."</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-    <a class=\"btn btn-primary\" href=\"#\">View Project</a>
-    </div>
-  </div>
-
-<!-- Modal -->
-<div class=\"modal fade\" id=\"modal".$row['codArticulo']."\" tabindex=\"-1\" aria-labelledby=\"modalLabel".$row['codArticulo']."\" aria-hidden=\"true\">
-  <div class=\"modal-dialog\">
-    <div class=\"modal-content\">
-      <div class=\"modal-header\">
-        <h1 class=\"modal-title fs-5\" id=\"exampleModalLabel\">".$row['nombre']."</h1>
-        <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button>
-      </div>
-      <div class=\"modal-body\">
-      <img class=\"img-fluid rounded mb-3 mb-md-0\" src=\"img/".$row['codArticulo'].".png\" alt=\"\" style=\"height:auto;width:600px\">
-      </div>
-      <div class=\"modal-footer\">
-        <button type=\"button\" class=\"btn btn-secondary\" data-bs-dismiss=\"modal\">Cerrar</button>
-      </div>
-    </div>
-  </div>
-</div>
-<hr style=\"margin-top:10px\">";
-}
+echo json_decode($result);
 }
 
