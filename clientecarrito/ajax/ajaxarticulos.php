bef1faaf0fdc8f58f2ef1d709788fe0ea2ac0ae0
RubenHerB <rubenhb19@educastur.es>
2024-03-11 17:55:56 +0000

    Mar 11, 2024, 6:55 PM

--- clientecarrito/ajax/ajaxarticulos.php
+++ clientecarrito/ajax/ajaxarticulos.php
@@ -1,14 +1,6 @@
 <?php
 if (isset($_POST)){
-    $filtro="";
-    if (isset($_POST['pocos'])){
-        if($_POST['pocos']==1){
-            $filtro=" WHERE cantidad<cantidadMinima";
-        }else{
-            $filtro=" WHERE NOT cantidad<cantidadMinima";
-        }
-    }
-    $query="SELECT * FROM `articulos`".$filtro;
+    $query="SELECT * FROM `articulos`";
     $connection = new mysqli('localhost', 'root','', 'carritocliente');
     if ($connection->connect_error){
         die("Fatal Error");
