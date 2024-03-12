1d329d632819e2751622cd77baf54d6b919986b2
RubenHerB <rubenhb19@educastur.es>
2024-03-12 23:11:06 +0000

    Mar 13, 2024, 12:11 AM

--- clientecarrito/ajax/ajaxedituser.php
+++ clientecarrito/ajax/ajaxedituser.php
@@ -1,7 +1,6 @@
 <?php
 if (isset($_POST)){
     $query="UPDATE `usuarios` SET `nombre`='".$_POST['nombre']."',`apellidos`='".$_POST['apellidos']."',`direccion`='".$_POST['direccion']."',`poblacion`='".$_POST['poblacion']."',`correo`='".$_POST['correo']."' WHERE DNI = '".$_POST['DNI']."'";
-    ";
     $connection = new mysqli('localhost', 'root','', 'carritocliente');
     if ($connection->connect_error){
         die("Fatal Error");
