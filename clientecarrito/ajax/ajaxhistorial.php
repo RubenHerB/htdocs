88f964dae952835833339ea75b83b044da0857be
RubenHerB <rubenhb19@educastur.es>
2024-03-13 07:34:21 +0000

    Mar 13, 2024, 8:34 AM

---
+++ clientecarrito/ajax/ajaxhistorial.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST)){
+    $query="SELECT * FROM `lineas` as `line` inner join `ventas' as v on v.codVenta = `line'.codVenta where v.DNI='".$_POST['dni']."'";
+    $connection = new mysqli('localhost', 'root','', 'carritocliente');
+    if ($connection->connect_error){
+        die("Fatal Error");
+    }
+$result = $connection->query($query);
+if (!$result){
+    die("Fatal Error");
+}
+$rows=$result->fetch_all(MYSQLI_ASSOC);
+echo json_encode($rows);
+}
+
