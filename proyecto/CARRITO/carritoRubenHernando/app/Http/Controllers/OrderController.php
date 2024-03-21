b219480a0a41ac33921d4bfb47373c7f067651fc
RubenHerB <rubenhb19@educastur.es>
2024-03-21 22:40:02 +0000

    Mar 21, 2024, 11:40 PM

--- proyecto/CARRITO/carritoRubenHernando/app/Http/Controllers/OrderController.php
+++ proyecto/CARRITO/carritoRubenHernando/app/Http/Controllers/OrderController.php
@@ -10,5 +10,11 @@ use App\Order;
 
 class OrderController extends Controller
 {
-    //
+    public function grabarCompra(){
+        $order=new Order();
+        $order->cart=serialize(Cart::content());
+        $order->direccion="desconocida";
+        $order->nombre=Auth::user()->name();
+        $order->idUsuario=Auth::user()->id();
+    }
 }
