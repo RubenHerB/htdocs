1b267d6f0f321996c9bfc722f4f5d1b647fbd515
RubenHerB <rubenhb19@educastur.es>
2024-03-21 21:37:34 +0000

    Mar 21, 2024, 10:37 PM

--- proyecto/CARRITO/carritoRubenHernando/resources/views/partials/msg.blade.php
+++ proyecto/CARRITO/carritoRubenHernando/resources/views/partials/msg.blade.php
@@ -2,4 +2,8 @@
     <div class="alert alert-success text-center">
         <p>{{\Session::get("success")}}</p>
     </div>
+@elseif(\Session::get("deny"))
+<div class="alert alert-danger text-center">
+        <p>{{\Session::get("deny")}}</p>
+    </div>
 @endif
