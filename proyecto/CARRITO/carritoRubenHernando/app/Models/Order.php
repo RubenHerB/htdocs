f98a88fbe93c105e045a37ba08ccae5f2d5157f5
RubenHerB <rubenhb19@educastur.es>
2024-03-21 22:47:08 +0000

    Mar 21, 2024, 11:47 PM

--- proyecto/CARRITO/carritoRubenHernando/app/Models/Order.php
+++ proyecto/CARRITO/carritoRubenHernando/app/Models/Order.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable=["idUsuario","carrito","direccion","nombre","id_pago"];
     public function user(){
         return $this->belongsTo('App\User');
     }
