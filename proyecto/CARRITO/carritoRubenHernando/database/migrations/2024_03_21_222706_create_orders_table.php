36f6d70a49a9227f078800da67fbcca0363b982d
RubenHerB <rubenhb19@educastur.es>
2024-03-21 22:30:44 +0000

    Mar 21, 2024, 11:30 PM

--- proyecto/CARRITO/carritoRubenHernando/database/migrations/2024_03_21_222706_create_orders_table.php
+++ proyecto/CARRITO/carritoRubenHernando/database/migrations/2024_03_21_222706_create_orders_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->integer('idUsuario');
+            $table->text('carrito');
+            $table->text('direccion');
+            $table->string('nombre');
+            $table->string('id_pago');
         });
     }
 
