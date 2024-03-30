23b91bc6366c27fa9e4680ed65b1f2997c7cd9f1
RubenHerB <rubenhb19@educastur.es>
2024-03-30 22:46:40 +0000

    Mar 30, 2024, 11:46 PM

---
+++ php_ordinaria/portfolio/ver_paciente.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="background-color:yellow; text-align:center;padding:15px; width:30%; margin-left:35%">LISTADO DE PACIENTES</h1>
+</body>
+</html>
