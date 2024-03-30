41ea287b1f256d10fd11eda7225869201e13c2f6
RubenHerB <rubenhb19@educastur.es>
2024-03-30 21:16:43 +0000

    Mar 30, 2024, 10:16 PM

--- php_ordinaria/portfolio/inicio.php
+++ php_ordinaria/portfolio/inicio.php
@@ -12,6 +12,19 @@
 <h1 style="background-color:yellow; text-align:center;padding:15px; width:30%; margin-left:35%"><?php echo $_SESSION['user']['tipo']; ?></h1>
 <?php 
 switch ($_SESSION['user']['tipo']){
+    case "Asistente":
+        echo <<<_END
+
+    _END;
+    break;
+    case "Medico":
+        $usu="medico";
+        $pass="medico";
+    break;
+    case "Administrador":
+        $usu="adminconsulta";
+        $pass="adminconsulta";
+    break;
 
 }
 ?>
