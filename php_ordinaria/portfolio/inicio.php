7b2831e9ec0397303530b4d611510cf54d4cddf3
RubenHerB <rubenhb19@educastur.es>
2024-03-30 21:25:49 +0000

    Mar 30, 2024, 10:25 PM

--- php_ordinaria/portfolio/inicio.php
+++ php_ordinaria/portfolio/inicio.php
@@ -4,6 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .button {
+    padding: 5px 6px;
+    border: 1px outset buttonborder;
+    border-radius: 3px;
+    color: buttontext;
+    background-color: buttonface;
+    text-decoration: none;
+    margin-left:auto;
+    margin-right:auto;
+}
+    </style>
 </head>
 <body>
     <?php 
@@ -14,29 +26,29 @@
 switch ($_SESSION['user']['tipo']){
     case "Asistente":
         echo <<<_END
-        <a href="citas_atentidas.php" type="button">Ver citas atendidas</a><br><br>
-        <a href="neva_cita.php" type="button">Nueva cita</a><br><br>
-        <a href="alta_paciente.php" type="button">Alta paciente</a><br><br>
-        <a href="ver_paciente.php" type="button">Ver pacientes</a><br><br>
+        <a href="citas_atentidas.php" class="button">Ver citas atendidas</a><br><br>
+        <a href="neva_cita.php" class="button">Nueva cita</a><br><br>
+        <a href="alta_paciente.php" class="button">Alta paciente</a><br><br>
+        <a href="ver_paciente.php" class="button">Ver pacientes</a><br><br>
     _END;
     break;
     case "Medico":
         echo <<<_END
         <a href="citas_atentidas.php" type="button">Ver citas atendidas</a><br><br>
-        <a href="citas_pendientes.php" type="button">Ver citas pendientes</a><br><br>
-        <a href="ver_paciente.php" type="button">Ver pacientes</a><br><br>
+        <a href="citas_pendientes.php" class="button">Ver citas pendientes</a><br><br>
+        <a href="ver_paciente.php" class="button">Ver pacientes</a><br><br>
     _END;
     break;
     case "Administrador":
         echo <<<_END
-        <a href="alta_paciente.php" type="button">Alta paciente</a><br><br>
-        <a href="alta_medico.php" type="button">Alta medico</a><br><br>
+        <a href="alta_paciente.php" class="button">Alta paciente</a><br><br>
+        <a href="alta_medico.php" class="button">Alta medico</a><br><br>
         _END;
     break;
 }
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-<input type="submit" value="Cerrar sesion" name="cierre" />
+<input type="submit" value="Cerrar sesion" name="cierre" class="button"/>
 </form>
 </body>
 </html>
