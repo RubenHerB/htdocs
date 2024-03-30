d833def8beaa0ad203566ce17b16ffa4bcfb576c
RubenHerB <rubenhb19@educastur.es>
2024-03-30 21:13:04 +0000

    Mar 30, 2024, 10:13 PM

--- php_ordinaria/portfolio/inicio.php
+++ php_ordinaria/portfolio/inicio.php
@@ -1,3 +1,14 @@
-<?php
-session_start();
-var_dump($_SESSION);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        session_start();
+    ?>
+<h1 style="background-color:yellow; text-align:center;padding:15px"><?php echo $_SESSION['user']['tipo']; ?></h1>
+</body>
+</html>
