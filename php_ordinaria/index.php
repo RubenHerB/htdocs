b68d4d939bd3fa9622385fa01ba2767c6bf9e688
RubenHerB <rubenhb19@educastur.es>
2024-03-30 15:59:17 +0000

    Mar 30, 2024, 4:59 PM

--- php_ordinaria/index.php
+++ php_ordinaria/index.php
@@ -7,6 +7,15 @@
 </head>
 <body>
     <h1 style="background-color:yellow; text-align:center;padding:15px">CENTRO MEDICO ASDI VIRTUAL</h1>
-    <div></div>
+    <div>
+        <form>
+            <h3 style="background-color:red; text-align:center;padding:15px;color:white">Inicio Sesion</h3>
+            Login: <input type="text" name="login" value="<?php echo $login ?>" ><br>
+            Password: <input type="password" name="password" value="<?php echo $password ?>" >
+            <div>
+                <input type="submit" value="Enviar">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
