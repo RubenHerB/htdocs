4c8b7a85fffd26bc0de1195a901092f34a077aa5
RubenHerB <rubenhb19@educastur.es>
2024-03-30 16:43:49 +0000

    Mar 30, 2024, 5:43 PM

--- php_ordinaria/index.php
+++ php_ordinaria/index.php
@@ -9,17 +9,26 @@
     <?php
         $login="";
         $password="";
+        $errorlog="";
         if(isset($_POST["login"])){
             $login=$_POST["login"];
             $password=$_POST["password"];
             include "portfolio/login.php";
             $conn=new Login();
+        if($login=""){
+
+        }
         }
     ?>
     <h1 style="background-color:yellow; text-align:center;padding:15px">CENTRO MEDICO ASDI VIRTUAL</h1>
     <div style="width:20%; text-align:center;margin-left:40%">
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
             <h3 style="background-color:red; text-align:center;padding:15px;color:white">Inicio Sesion</h3>
+            <?php
+            if($errorlog!=""){
+                echo "<p style=\"color:red\">$errorlog</p>";
+            }
+            ?>
             Login: <input type="text" name="login" value="<?php echo $login ?>" ><br>
             Password: <input type="password" name="password" value="<?php echo $password ?>" >
             <div style="background-color:red; text-align:center;padding:5px 15px;margin-top:5px">
