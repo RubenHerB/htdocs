d4b8d3e81851731aac432bee608862c111495fa1
RubenHerB <rubenhb19@educastur.es>
2024-03-31 19:25:39 +0000

    Mar 31, 2024, 9:25 PM

--- php_ordinaria/atender_cita.php
+++ php_ordinaria/atender_cita.php
@@ -6,6 +6,16 @@
     <title>Document</title>
 </head>
 <body>
-echo "<p style=\"color:red\">$errorlog</p>";
+<?php
+if(isset($_POST['citaatendida'])){
+echo "<p style=\"color:red\">Error! La casilla de observaciones no puede estar vacia</p>";
+}
+?>
+<div style="width:20%; text-align:center;margin-left:40%">
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+            <h3 style="background-color:red; text-align:center;padding:15px;color:white">ATENDER CITA</h3>
+            Paciente:
+        </form>
+</div>
 </body>
 </html>
