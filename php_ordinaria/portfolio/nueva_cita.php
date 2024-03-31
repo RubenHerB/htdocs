efb44b70fb07e8dec64bdde1f723e75513e2b81b
RubenHerB <rubenhb19@educastur.es>
2024-03-31 16:24:23 +0000

    Mar 31, 2024, 6:24 PM

--- php_ordinaria/portfolio/nueva_cita.php
+++ php_ordinaria/portfolio/nueva_cita.php
@@ -10,15 +10,11 @@
 <h1 style="background-color:yellow; text-align:center;padding:15px">ASDI VIRTUAL</h1>
 <div style="width:20%; text-align:center;margin-left:40%">
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-            <h3 style="background-color:green; text-align:center;padding:15px;color:white">Alta de paciente</h3>
+            <h3 style="background-color:green; text-align:center;padding:15px;color:white">ASIGNAR CITA</h3>
             Identificacion: <input type="text" name="id"  required><br>
             Nombre: <input type="text" name="nombre"  required><br>
             Apellidos: <input type="text" name="apellidos"  required><br>
             Fecha nacimiento: <input type="date" name="fecha" min='<?php echo date('Y-m-d'); ?>' required><br>
-            Sexo:<select name="sexo">
-                    <option value="Femenino" selected>Femenino</option>
-                    <option value="Masculino">Masculino</option>
-                </select>
             <div style="background-color:green; text-align:center;padding:5px 15px;margin-top:5px">
                 <input type="submit" name="altap" value="Enviar">
             </div>
