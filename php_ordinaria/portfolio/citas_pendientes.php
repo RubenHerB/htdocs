010d450115db16af36c22c317bde99b4ac0b0684
RubenHerB <rubenhb19@educastur.es>
2024-03-31 19:11:40 +0000

    Mar 31, 2024, 9:11 PM

--- php_ordinaria/portfolio/citas_pendientes.php
+++ php_ordinaria/portfolio/citas_pendientes.php
@@ -25,27 +25,27 @@
             <th>Hora</th>
             <th>Paciente</th>
             <?php
+            session_start();
             if($_SESSION['user']['tipo']!="Medico"){
             echo "<th>Medico</th>";
             }
             ?>
             <th>Consultorio</th>
-            <th colspan="3">Observaciones</th>
+            <th>Observaciones</th>
         </tr>
         <?php
-        session_start();
         if($_SESSION['user']['tipo']=="Medico"){
-            $query="SELECT c.citFecha as fecha,c.citHora as hora,p.pacNombre as nombrep,p.pacApellidos as apep,con.conNombre as nombrec,c.CitObservaciones as observaciones
+            $query="SELECT c.citFecha as fecha,c.citHora as hora,p.pacNombres as nombrep,p.pacApellidos as apep,con.conNombre as nombrec,c.CitObservaciones as observaciones
             FROM citas AS c
-            inner join pacientes as p on c.citPaciente=m.dniPac
-            inner join consultorios as con on c.citConsultorio=m.idConsultorio
+            inner join pacientes as p on c.citPaciente=p.dniPac
+            inner join consultorios as con on c.citConsultorio=con.idConsultorio
             WHERE c.citEstado LIKE 'Atendido' AND c.citMedico LIKE '".$_SESSION['user']['dni']."'";
         }else{
-            $query="SELECT c.citFecha as fecha,c.citHora as hora,p.pacNombre as nombrep,p.pacApellidos as apep,m.medNombre as nombrem, m.medApellidos as apem, con.conNombre as nombrec,c.CitObservaciones as observaciones
+            $query="SELECT c.citFecha as fecha,c.citHora as hora,p.pacNombres as nombrep,p.pacApellidos as apep,m.medNombres as nombrem, m.medApellidos as apem, con.conNombre as nombrec,c.CitObservaciones as observaciones
             FROM citas AS c
             inner join medicos as m on c.citMedico=m.dniMed
-            inner join pacientes as p on c.citPaciente=m.dniPac
-            inner join consultorios as con on c.citConsultorio=m.idConsultorio
+            inner join pacientes as p on c.citPaciente=p.dniPac
+            inner join consultorios as con on c.citConsultorio=con.idConsultorio
             WHERE c.citEstado LIKE 'Atendido'";
         }
         include "login.php";
@@ -55,7 +55,7 @@
         if (!$result) die("Fatal Error");
 
         foreach($result as $r){
-            echo "<tr><td>".$r["fecha"]."</td><td>".$r["hora"]."</td><td>".$r["nombrep"]." ".$r["apep"].($_SESSION['user']['tipo']=="Medico"?("</td><td>".$r["nombrem"]." ".$r["apem"]):"")."</td><td>".$r["nombrec"]."</td><td>".$r["observaciones"]."</td></tr>";
+            echo "<tr><td>".$r["fecha"]."</td><td>".$r["hora"]."</td><td>".$r["nombrep"]." ".$r["apep"].($_SESSION['user']['tipo']!="Medico"?("</td><td>".$r["nombrem"]." ".$r["apem"]):"")."</td><td>".$r["nombrec"]."</td><td>".$r["observaciones"]."</td></tr>";
 
         }
         ?>
