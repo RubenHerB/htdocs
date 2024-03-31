57fb8d5b75290d4d1a0575f6c5bc7f02c3c1158e
RubenHerB <rubenhb19@educastur.es>
2024-03-31 16:27:41 +0000

    Mar 31, 2024, 6:27 PM

--- php_ordinaria/portfolio/nueva_cita.php
+++ php_ordinaria/portfolio/nueva_cita.php
@@ -11,11 +11,20 @@
 <div style="width:20%; text-align:center;margin-left:40%">
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
             <h3 style="background-color:red; text-align:center;padding:15px;color:white">ASIGNAR CITA</h3>
-            Paciente: <select name="paciente" selected>
+            Paciente: <select name="paciente" required>
+                    <option value="Femenino">Femenino</option>
+                    <option value="Masculino">Masculino</option>
+                </select><br>
+            Fecha: <input type="date" name="fecha" min='<?php echo date('Y-m-d'); ?>' required><br>
+            Hora: <input type="time" name="hora" min='<?php echo date('Y-m-d'); ?>' required><br>
+            Medico: <select name="medico" required>
+                    <option value="Femenino">Femenino</option>
+                    <option value="Masculino">Masculino</option>
+                </select><br>
+            Consultorio: <select name="consultorio" required>
                     <option value="Femenino">Femenino</option>
                     <option value="Masculino">Masculino</option>
                 </select>
-            Fecha: <input type="date" name="fecha" min='<?php echo date('Y-m-d'); ?>' required><br>
             <div style="background-color:red; text-align:center;padding:5px 15px;margin-top:5px">
                 <input type="submit" name="nuevac" value="Enviar">
             </div>
