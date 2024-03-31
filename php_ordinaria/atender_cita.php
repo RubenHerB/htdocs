48a28987ad1aed935da5824defb2f15a99c7cbee
RubenHerB <rubenhb19@educastur.es>
2024-03-31 19:30:04 +0000

    Mar 31, 2024, 9:30 PM

--- php_ordinaria/atender_cita.php
+++ php_ordinaria/atender_cita.php
@@ -16,7 +16,12 @@ echo "<p style=\"color:red\">Error! La casilla de observaciones no puede estar v
 <div style="width:20%; text-align:center;margin-left:40%">
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
             <h3 style="background-color:red; text-align:center;padding:15px;color:white">ATENDER CITA</h3>
-            Paciente:
+            Paciente: <?php echo $_POST["nombrepac"];?><br>
+            <input type="hidden" name="id" value="<?php echo $_POST["id"];?>">
+            Observaciones: 
+            <div style="background-color:red; text-align:center;padding:5px 15px;margin-top:5px">
+                <input type="submit" name="citaatendida" value="Enviar">
+            </div>
         </form>
 </div>
 </body>
